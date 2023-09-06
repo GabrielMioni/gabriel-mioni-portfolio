08897903c34f68ec51e0b3b0ce2ebb9b43e26f2c
Gabriel Mioni <gabriel.mioni@gmail.com>
2023-09-05 19:29:01 -0700

    App uses Vuetify layout

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,47 +1,21 @@
 <script setup>
-import HelloWorld from '@/components/HelloWorld.vue'
-import Baba from '@/components/Baba.vue'
 import ProjectsList from '@/components/ProjectsList.vue'
 </script>
 
 <template>
-  <div>
-    <Baba />
-    <projects-list />
-    <a
-      href="https://vitejs.dev"
-      target="_blank">
-      <img
-        src="/vite.svg"
-        class="logo extra"
-        alt="Vite logo">
-    </a>
-    <a
-      href="https://vuejs.org/"
-      target="_blank">
-      <img
-        src="./assets/vue.svg"
-        class="logo vue"
-        alt="Vue logo">
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <v-app>
+    <v-main>
+      <v-container>
+        <v-row>
+          <v-col>
+            <projects-list />
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <style lang="scss" scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-  &.extra {
-    border: 1px solid blue;
-  }
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>

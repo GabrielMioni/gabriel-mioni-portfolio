14a97482d45f9e8f406e42481fb6113cecb706b3
Gabriel Mioni <gabriel.mioni@gmail.com>
2023-09-07 18:24:02 -0700

    Added hero image in App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,19 @@
 <script setup>
 import ProjectsList from '@/components/ProjectsList.vue'
+import heroImage from '@/assets/nature-forest-trees-fog.jpeg'
 </script>
 
 <template>
   <v-app>
     <v-main>
+      <v-parallax
+        :src="heroImage"
+        height="300">
+        <div class="d-flex flex-column fill-height justify-center text-white text-h4 text-center">
+          Web developer.<br>
+          Available for hire.
+        </div>
+      </v-parallax>
       <v-container>
         <v-row>
           <v-col>

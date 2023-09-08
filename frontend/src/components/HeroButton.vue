4527654d2cafa19aeca9b470676f25959f447f0e
Gabriel Mioni <gabriel.mioni@gmail.com>
2023-09-07 19:25:29 -0700

    Added HeroButton

---
+++ frontend/src/components/HeroButton.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-btn
+    color="transparent"
+    elevation="0"
+    rounded="0"
+    class="hero-button">
+    <span class="text-white pa-2">
+      <slot />
+    </span>
+  </v-btn>
+</template>
+
+<script>
+export default {
+  name: 'HeroButton'
+}
+</script>
+
+<style lang="scss" scoped>
+.hero-button {
+  border: 2px solid #fff;
+}
+</style>

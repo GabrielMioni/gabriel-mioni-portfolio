dc9cfe2e0736d0560254df1373222f620bf9cb56
Gabriel Mioni <gabriel.mioni@gmail.com>
2023-09-12 18:46:41 -0700

    Added AboutView and HomeView

---
+++ frontend/src/views/AboutView.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-container>
+    <v-row style="justify-content: center">
+      <v-col :cols="8">
+        This is the about page.
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'AboutView'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<script setup>
+import ProjectsList from '@/components/ProjectsList.vue'
+</script>
+
+<template>
+  <v-container>
+    <v-row style="justify-content: center">
+      <v-col :cols="8">
+        <projects-list />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<style scoped>
+
+</style>

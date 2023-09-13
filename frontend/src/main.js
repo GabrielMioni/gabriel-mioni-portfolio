9b727a7ae7c5eb8e585ddca50366e560c6d119e4
Gabriel Mioni <gabriel.mioni@gmail.com>
2023-09-12 17:59:03 -0700

    In main.js, setup mdi fonts

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,13 +3,22 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 
 import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
 const vuetify = createVuetify({
   components,
-  directives
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi
+    }
+  },
 })
 
 const app = createApp(App)

6662093afbf8940f15ba00d1542871e8101c12e8
Gabriel Mioni <gabriel.mioni@gmail.com>
2023-09-13 19:50:24 -0700

    Added FooterSection

---
+++ frontend/src/components/FooterSection.vue
@@ -0,0 +1,26 @@
+<script setup>
+import profilePic from '@/assets/saturn-eating-his-son.png'
+import ContactForm from '@/components/ContactForm.vue'
+
+</script>
+
+<template>
+  <v-footer color="#036870">
+    <v-container>
+      <v-row align="center">
+        <v-col
+          :cols="3"
+          class="d-flex justify-center align-center">
+          <v-avatar size="200px">
+            <v-img
+              :src="profilePic"
+              cover />
+          </v-avatar>
+        </v-col>
+        <v-col>
+          <contact-form />
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-footer>
+</template>

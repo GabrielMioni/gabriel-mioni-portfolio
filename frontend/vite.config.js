c9a933afa96b013155a4bc24bf94286b280062ff
Gabriel Mioni <gabriel.mioni@gmail.com>
2023-09-14 20:21:38 -0700

    Installed @rollup/plugin-graphql

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,13 +2,15 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import { fileURLToPath } from 'url'
+import graphql from '@rollup/plugin-graphql'
+
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), graphql()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')

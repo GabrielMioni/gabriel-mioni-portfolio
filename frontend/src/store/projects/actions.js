f0c744e37569c542cb006b3140932079739828d6
Gabriel Mioni <gabriel.mioni@gmail.com>
2023-09-18 20:35:29 -0700

    In actions.js, methods actually update state

--- frontend/src/store/projects/actions.js
+++ frontend/src/store/projects/actions.js
@@ -1,5 +1,17 @@
+import { fetchAllProjects } from '@/services/projectsService.js'
+
 export const actions = {
-  addProject (state, project) {
-    state.projects.push(project)
+  addProject (project) {
+    this.PROJECTS.push(project)
+  },
+  async loadProjects () {
+    const projects = await fetchAllProjects().catch(e => console.error(e))
+    if (projects.length <= 0) {
+      console.warn('No projects loaded')
+    }
+    this.PROJECTS = projects
+  },
+  setProjectsLoading (value) {
+    this.PROJECTS_LOADING = value
   }
 }

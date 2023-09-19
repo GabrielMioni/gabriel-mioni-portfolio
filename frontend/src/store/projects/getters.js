3e34c559350dc344ea2449b5eb7daa4f295363ac
Gabriel Mioni <gabriel.mioni@gmail.com>
2023-09-18 20:36:34 -0700

    Added projectsFormatted getter in getters.js

--- frontend/src/store/projects/getters.js
+++ frontend/src/store/projects/getters.js
@@ -1,3 +1,11 @@
 export const getters = {
-  projects: (state) => state.projectsState
+  projects: (state) => state.PROJECTS,
+  projectsLoading: (state) => state.PROJECTS_LOADING,
+  projectsFormatted: (state) => {
+    return state.PROJECTS.map(project => {
+      const { description } = project
+      const formattedDescription = description ? description.split('\n\n') : []
+      return { ...project, formattedDescription }
+    })
+  }
 }

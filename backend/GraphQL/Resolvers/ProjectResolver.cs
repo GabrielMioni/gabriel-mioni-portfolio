fc805d9c74d0c5dfca613c21e6ba7ae32e37d4f5
Gabriel Mioni <gabriel.mioni@gmail.com>
2023-09-20 20:05:12 -0700

    Hard coded Projects returns null Image Property

--- backend/GraphQL/Resolvers/ProjectResolver.cs
+++ backend/GraphQL/Resolvers/ProjectResolver.cs
@@ -13,7 +13,7 @@ namespace backend.GraphQL.Resolvers
         {
             _projects = new List<Project>
             {
-                new Project { Id = 1, Name = "React Checkers", Description = "This 2-player checkers game...", Image = "reactCheckers", Git = "https://github.com/GabrielMioni/react-checkers" },
+                new Project { Id = 1, Name = "React Checkers", Description = "This 2-player checkers game...", Image = null, Git = "https://github.com/GabrielMioni/react-checkers" },
                 new Project { Id = 2, Name = "Project B", Description = "This is a description for Project B.", Image = null, Git = null },
                 new Project { Id = 3, Name = "Project C", Description = "This is a description for Project C.", Image = null, Git = null }
             };

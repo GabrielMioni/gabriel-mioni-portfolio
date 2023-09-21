be499c88f144dd65026a0d0171d8b36bc6623a77
Gabriel Mioni <gabriel.mioni@gmail.com>
2023-09-20 20:03:38 -0700

    Added Image model

---
+++ backend/Models/Image.cs
@@ -0,0 +1,18 @@
+﻿namespace backend.Models
+{
+    public class Image
+    {
+        public int Id { get; set; }
+        public string FileName { get; set; }
+        public string ContentType { get; set; }
+        public string AltText { get; set; }
+        public string Title { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+        public long Size { get; set; }
+
+        // Navigational properties
+        public int ProjectId { get; set; }
+        public Project Project { get; set; }
+    }
+}

2473afe20d50c6c73a3700f9edc44b72df62310b
Gabriel Mioni <gabriel.mioni@gmail.com>
2023-09-23 13:09:02 -0700

    Added ProjectsRepository and related interface

---
+++ backend/Features/Projects/GraphQL/IProjectResolver.cs
@@ -0,0 +1,9 @@
+﻿using backend.Features.Projects.Models;
+
+namespace backend.Features.Projects.GraphQL
+{
+    public interface IProjectResolver
+    {
+        public Task<IEnumerable<Project>> GetAllProjectsAsync();
+    }
+}

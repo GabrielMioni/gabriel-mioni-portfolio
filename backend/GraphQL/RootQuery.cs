26fccb367cb9d8d1f1a704ef167e8dbff0a4d045
Gabriel Mioni <gabriel.mioni@gmail.com>
2023-09-23 13:10:32 -0700

    RootQuery injects IProjectResolver

--- backend/GraphQL/RootQuery.cs
+++ backend/GraphQL/RootQuery.cs
@@ -1,21 +1,24 @@
-﻿using backend.Models;
+﻿using backend.Features.Projects.GraphQL;
+using backend.Features.Projects.Models;
 
 namespace backend.GraphQL
 {
     public class RootQuery
     {
+        private readonly IProjectResolver _projectResolver;
+
+        public RootQuery(IProjectResolver projectResolver)
+        {
+            _projectResolver = projectResolver;
+        }
+
         [GraphQLDescription("A simple hello world query")]
         public string HelloWorld() => "Hello, world!";
 
         [GraphQLDescription("Retrieve the list of projects")]
-        public List<Project> GetProjects()
+        public async Task<List<Project>> GetProjects()
         {
-            return new List<Project>
-            {
-                new Project { Id = 1, Name = "React Checkers", Description = "This 2-player checkers game, built in React.js, offers a mobile-friendly design and a board that maintains a state history, allowing players to undo moves for a seamless gaming experience. The rules deviate slightly from traditional tournament checkers; you aren't obligated to make jumps, giving you more strategic freedom.\n\nThe game's logic resides primarily in the ReactCheckers component, where the board and its pieces are represented as objects. As players make their moves, the board state is recorded, making it easy to undo actions. Victory conditions are checked at the end of each turn, giving you a game that is as challenging as it is enjoyable.", Image = null, Git = "https://github.com/GabrielMioni/react-checkers" },
-                new Project { Id = 2, Name = "Project B", Description = "This is a description for Project B.", Image = null, Git = null },
-                new Project { Id = 3, Name = "Project C", Description = "This is a description for Project C.", Image = null, Git = null }
-            };
+            return (await _projectResolver.GetAllProjectsAsync()).ToList();
         }
     }
 }

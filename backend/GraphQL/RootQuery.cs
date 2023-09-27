559c06214d906f8026d697084ca4491778d951f7
Gabriel Mioni <gabriel.mioni@gmail.com>
2023-09-26 18:38:35 -0700

    In RootQuery, inject iProjectsRepository through HotChocolate Service decorator on GetProjectsAsync method

--- backend/GraphQL/RootQuery.cs
+++ backend/GraphQL/RootQuery.cs
@@ -1,24 +1,17 @@
-﻿using backend.Features.Projects.GraphQL;
-using backend.Features.Projects.Models;
+﻿using backend.Features.Projects.Models;
+using backend.Features.Projects.Repositories;
 
 namespace backend.GraphQL
 {
     public class RootQuery
     {
-        private readonly IProjectResolver _projectResolver;
-
-        public RootQuery(IProjectResolver projectResolver)
-        {
-            _projectResolver = projectResolver;
-        }
-
         [GraphQLDescription("A simple hello world query")]
         public string HelloWorld() => "Hello, world!";
 
         [GraphQLDescription("Retrieve the list of projects")]
-        public async Task<List<Project>> GetProjects()
+        public async Task<List<Project>> GetProjectsAsync([Service]IProjectsRepository repository)
         {
-            return (await _projectResolver.GetAllProjectsAsync()).ToList();
+            return await repository.GetAllProjects();
         }
     }
 }

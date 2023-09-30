1efb5d2ede25b29b170c5bcef21a4bdd96ae9d96
Gabriel Mioni <gabriel.mioni@gmail.com>
2023-09-30 10:11:05 -0700

    Added ProjectInput

---
+++ backend/Features/Projects/Models/ProjectInput.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backend.Features.Projects.Models
+{
+    [InputObjectType]
+    public class ProjectInput
+    {
+        [GraphQLNonNullType]
+        [Required]
+        public string Name { get; set; }
+
+        [GraphQLNonNullType]
+        [Required]
+        public string Description { get; set; }
+        public string? Git { get; set; }
+    }
+}

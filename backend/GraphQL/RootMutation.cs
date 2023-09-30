5fd56dd5af3aead9619849c056e9309e6029323f
Gabriel Mioni <gabriel.mioni@gmail.com>
2023-09-30 10:09:17 -0700

    Added RootMutation.cs

---
+++ backend/GraphQL/RootMutation.cs
@@ -0,0 +1,21 @@
+﻿using backend.Features.Projects.Models;
+using backend.Features.Projects.Repositories;
+
+namespace backend.GraphQL
+{
+    public class RootMutation
+    {
+        public async Task<Project> AddProjectAsync(
+            ProjectInput input,
+            [Service] IProjectsRepository repository)
+        {
+            Project newProject = new Project {
+                Name = input.Name,
+                Description = input.Description,
+                Git = input.Git,
+            };
+
+            return await repository.AddProjectAsync(newProject);
+        }
+    }
+}

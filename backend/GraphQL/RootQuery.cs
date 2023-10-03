f57cfb894423918e9f077e1368381e9097d6e68f
Gabriel Mioni <gabriel.mioni@gmail.com>
2023-10-02 19:34:00 -0700

    In RootQuery, added error handling for GetProjectListAsync

--- backend/GraphQL/RootQuery.cs
+++ backend/GraphQL/RootQuery.cs
@@ -1,4 +1,5 @@
-﻿using backend.Features.Projects.Models;
+﻿using backend.Features.Projects.GraphQL;
+// using backend.Features.Projects.Models;
 using backend.Features.Projects.Repositories;
 
 namespace backend.GraphQL
@@ -9,9 +10,21 @@ namespace backend.GraphQL
         public string HelloWorld() => "Hello, world!";
 
         [GraphQLDescription("Retrieve the list of projects")]
-        public async Task<List<Project>> GetProjectsAsync([Service]IProjectsRepository repository)
+        public async Task<ProjectResult> GetProjectListAsync([Service]IProjectsRepository repository)
         {
-            return await repository.GetAllProjects();
+            var result = new ProjectResult();
+            result.Errors = new List<string>();
+            try
+            {
+                var projects = await repository.GetAllProjectsAsync();
+                result.Projects = projects;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                result.Errors = new List<string> { "An error occurred while fetching projects." };
+            }
+            return result;
         }
     }
 }

98b51c4332c99e14dc1a6707de3108d51ec514c6
Gabriel Mioni <gabriel.mioni@gmail.com>
2023-10-02 19:12:46 -0700

    Added ProjectResultType.cs

---
+++ backend/Features/Projects/GraphQL/ProjectResultType.cs
@@ -0,0 +1,22 @@
+﻿using backend.Features.Projects.Models;
+
+namespace backend.Features.Projects.GraphQL
+{
+    public class ProjectResultType : ObjectType<ProjectResult>
+    {
+        protected override void Configure(IObjectTypeDescriptor<ProjectResult> descriptor)
+        {
+            descriptor.Description("Represents the result of a project query");
+
+            descriptor
+                .Field(p => p.Projects)
+                .Description("Projects")
+                .Type<Project>();
+
+            descriptor
+                .Field(p => p.Errors)
+                .Description("Any errors that occurred during the query")
+                .Type<ListType<NonNullType<StringType>>>();
+        }
+    }
+}

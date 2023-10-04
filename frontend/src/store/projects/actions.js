01b585ba7b8ca5d4c1b659c935bbf5daff5a078a
Gabriel Mioni <gabriel.mioni@gmail.com>
2023-10-03 19:16:37 -0700

    In projects Pinia store, loadProjects accepts object with skip, take properties

--- frontend/src/store/projects/actions.js
+++ frontend/src/store/projects/actions.js
@@ -1,11 +1,11 @@
-import { fetchAllProjects } from '@/services/projectsService.js'
+import { fetchProjects } from '@/services/projectsService.js'
 
 export const actions = {
   addProject (project) {
     this.PROJECTS.push(project)
   },
-  async loadProjects () {
-    const projects = await fetchAllProjects().catch(e => console.error(e))
+  async loadProjects ({ skip, take }) {
+    const projects = await fetchProjects({ skip, take }).catch(e => console.error(e))
     if (projects.length <= 0) {
       console.warn('No projects loaded')
     }

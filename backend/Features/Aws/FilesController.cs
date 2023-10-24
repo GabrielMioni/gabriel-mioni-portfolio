8c1f18c8e3201c240e9fe273b8605681975a82f3
Gabriel Mioni <gabriel.mioni@gmail.com>
2023-10-23 18:44:46 -0700

    in FilesController, inject IAmazonS3 through constructor

--- backend/Features/Aws/FilesController.cs
+++ backend/Features/Aws/FilesController.cs
@@ -11,21 +11,16 @@ namespace backend.Features.Aws
     [ApiController]
     public class FilesController : ControllerBase
     {
-        private readonly IConfiguration _configuration;
-        public FilesController(IConfiguration configuration)
+        private readonly IAmazonS3 _amazonS3;
+
+        public FilesController(IAmazonS3 amazonS3)
         {
-            _configuration = configuration;
+            _amazonS3 = amazonS3;            
         }
 
         [HttpPost]
         public async Task<IActionResult> UploadFileAsync(IFormFile file, string? prefix)
         {
-            var accessKey = _configuration.GetValue<string>("AWS:AccessKey");
-            var secretKey = _configuration.GetValue<string>("AWS:SecretKey");
-            var regionString = _configuration.GetValue<string>("AWS:Region");
-
-            var region = RegionEndpoint.GetBySystemName(regionString);
-
             var request = new PutObjectRequest()
             {
                 BucketName = "portfolio-gjam-dev",
@@ -33,28 +28,20 @@ namespace backend.Features.Aws
                 InputStream = file.OpenReadStream()
             };
             request.Metadata.Add("Content-Type", file.ContentType);
-            var s3Client = new AmazonS3Client(accessKey, secretKey, region);
-            await s3Client.PutObjectAsync(request);
+            await _amazonS3.PutObjectAsync(request);
             return Ok($"File {file.FileName} uploaded to S3 successfully");
         }
 
         [HttpGet]
         public async Task<IActionResult> GetAllFilesAsync()
         {
-            var accessKey = _configuration.GetValue<string>("AWS:AccessKey");
-            var secretKey = _configuration.GetValue<string>("AWS:SecretKey");
-            var regionString = _configuration.GetValue<string>("AWS:Region");
-
-            var region = RegionEndpoint.GetBySystemName(regionString);
-
-            var s3Client = new AmazonS3Client(accessKey, secretKey, region);
             var request = new ListObjectsV2Request()
             {
                 BucketName = "portfolio-gjam-dev",
                 Prefix = null
             };
 
-            var result = await s3Client.ListObjectsV2Async(request);
+            var result = await _amazonS3.ListObjectsV2Async(request);
             var s3Objects = result.S3Objects.Select(s =>
             {
                 var urlRequest = new GetPreSignedUrlRequest()
@@ -66,7 +53,7 @@ namespace backend.Features.Aws
                 return new S3ObjectDto()
                 {
                     Name = s.Key.ToString(),
-                    PresignedUrl = s3Client.GetPreSignedURL(urlRequest)
+                    PresignedUrl = _amazonS3.GetPreSignedURL(urlRequest)
                 };
             });
             return Ok(s3Objects);

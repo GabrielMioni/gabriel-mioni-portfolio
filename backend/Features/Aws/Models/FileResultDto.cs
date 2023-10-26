ae8adccae972b8a91d18ce7eced849a51e0610b1
Gabriel Mioni <gabriel.mioni@gmail.com>
2023-10-25 19:46:10 -0700

    Added FileResultDto and FileUploadResultDto

---
+++ backend/Features/Aws/Models/FileResultDto.cs
@@ -0,0 +1,8 @@
+﻿namespace backend.Features.Aws.Models
+{
+    public class FileResultDto
+    {
+        public Stream FileStream { get; set; }
+        public string ContentType { get; set; }
+    }
+}
---
+++ backend/Features/Aws/Models/FileUploadResultDto.cs
@@ -0,0 +1,8 @@
+﻿namespace backend.Features.Aws.Models
+{
+    public class FileUploadResultDto
+    {
+        public string S3Key { get; set; }
+        public string DbRecordId { get; set; }
+    }
+}

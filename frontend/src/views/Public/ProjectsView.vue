894eb906f2a2331b57dcad6e0db1e5681c7e3a24
Gabriel Mioni <gabriel.mioni@gmail.com>
2023-11-02 18:31:39 -0700

    In ProjectsView, v-card and v-pagination are nested in parent div

--- frontend/src/views/Public/ProjectsView.vue
+++ frontend/src/views/Public/ProjectsView.vue
@@ -32,52 +32,54 @@ onMounted(() => {
 </script>
 
 <template>
-  <v-card
-    v-for="project in projects"
-    :key="project.id"
-    class="mb-4"
-    elevation="1">
-    <v-card-title>
-      {{ project.name }}
-    </v-card-title>
-    <v-card-text>
-      <v-container>
-        <v-row>
-          <v-col
-            v-if="project.image"
-            :cols="4">
-            <v-img
-              :src="project.image"
-              max-width="100%" />
-          </v-col>
-          <v-col class="d-flex align-center">
-            <div>
-              <div
-                v-for="(paragraph, i) in project.formattedDescription"
-                :key="i"
-                class="mb-3">
-                {{ paragraph }}
+  <div>
+    <v-card
+      v-for="project in projects"
+      :key="project.id"
+      class="mb-4"
+      elevation="1">
+      <v-card-title>
+        {{ project.name }}
+      </v-card-title>
+      <v-card-text>
+        <v-container>
+          <v-row>
+            <v-col
+              v-if="project.image"
+              :cols="4">
+              <v-img
+                :src="project.image"
+                max-width="100%" />
+            </v-col>
+            <v-col class="d-flex align-center">
+              <div>
+                <div
+                  v-for="(paragraph, i) in project.formattedDescription"
+                  :key="i"
+                  class="mb-3">
+                  {{ paragraph }}
+                </div>
+                <a
+                  v-if="project.git"
+                  :href="project.git"
+                  class="github-link text-decoration-none"
+                  target="_blank">
+                  <v-icon icon="mdi-github" />
+                  <span class="font-italic font-weight-bold">
+                    See it on Github
+                  </span>
+                </a>
               </div>
-              <a
-                v-if="project.git"
-                :href="project.git"
-                class="github-link text-decoration-none"
-                target="_blank">
-                <v-icon icon="mdi-github" />
-                <span class="font-italic font-weight-bold">
-                  See it on Github
-                </span>
-              </a>
-            </div>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-card-text>
-  </v-card>
-  <v-pagination
-    v-model="currentPage"
-    :length="totalPages"
-    :total-visible="5" />
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card-text>
+    </v-card>
+    <v-pagination
+      v-model="currentPage"
+      :length="totalPages"
+      :total-visible="5" />
+  </div>
 </template>
 
 <style lang="scss" scoped>

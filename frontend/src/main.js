b05279195b6269fac5b0ca6441e939821a06c791
Gabriel Mioni <gabriel.mioni@gmail.com>
2023-11-14 18:15:15 -0800

    In main.js, added PrimeVue. Added DataTable and Column components

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -9,9 +9,17 @@ import 'vuetify/styles'
 import '@mdi/font/css/materialdesignicons.css'
 import { aliases, mdi } from 'vuetify/iconsets/mdi'
 import { createVuetify } from 'vuetify'
+import PrimeVue from 'primevue/config'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 import { VDataTableServer } from 'vuetify/labs/VDataTable'
+
+import 'primevue/resources/themes/lara-light-indigo/theme.css'
+import 'primevue/resources/primevue.min.css'
+import 'primeicons/primeicons.css'
+
+import DataTable from 'primevue/datatable'
+import Column from 'primevue/column'
 
 const vuetify = createVuetify({
   components: {
@@ -36,6 +44,11 @@ app.provide(DefaultApolloClient, apolloClient)
 
 app.use(pinia)
 app.use(vuetify)
+app.use(PrimeVue)
 app.use(router)
+
+app.component('DataTable', DataTable)
+// eslint-disable-next-line vue/multi-word-component-names
+app.component('Column', Column)
 
 app.mount('#app')

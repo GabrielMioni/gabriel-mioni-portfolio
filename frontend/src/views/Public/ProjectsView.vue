223f3ab8f909901a8438a9ad6b2b0c542d2a4fee
Gabriel Mioni <gabriel.mioni@gmail.com>
2023-11-27 17:52:06 -0800

    ProjectsView uses PrimeVue Card instead of Vuetify v-card

--- frontend/src/views/Public/ProjectsView.vue
+++ frontend/src/views/Public/ProjectsView.vue
@@ -33,48 +33,45 @@ onMounted(() => {
 
 <template>
   <div>
-    <v-card
+    <card
       v-for="project in projects"
       :key="project.id"
-      class="mb-4"
-      elevation="1">
-      <v-card-title>
-        {{ project.name }}
-      </v-card-title>
-      <v-card-text>
-        <v-container>
-          <v-row>
-            <v-col
-              v-if="project.image"
-              :cols="4">
-              <v-img
-                :src="project.image"
-                max-width="100%" />
-            </v-col>
-            <v-col class="d-flex align-center">
-              <div>
-                <div
-                  v-for="(paragraph, i) in project.formattedDescription"
-                  :key="i"
-                  class="mb-3">
-                  {{ paragraph }}
-                </div>
-                <a
-                  v-if="project.git"
-                  :href="project.git"
-                  class="github-link text-decoration-none"
-                  target="_blank">
-                  <v-icon icon="mdi-github" />
-                  <span class="font-italic font-weight-bold">
-                    See it on Github
-                  </span>
-                </a>
-              </div>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-card-text>
-    </v-card>
+      class="mb-3">
+      <template #title>
+        <span class="text-lg">
+          {{ project.name }}
+        </span>
+      </template>
+      <template #content>
+        <div class="grid">
+          <div
+            v-if="project.image"
+            class="col-4">
+            <image
+              :src="project.image"
+              alt="project image" />
+          </div>
+          <div class="col">
+            <div
+              v-for="(paragraph, i) in project.formattedDescription"
+              :key="i"
+              class="mb-3">
+              {{ paragraph }}
+            </div>
+            <a
+              v-if="project.git"
+              :href="project.git"
+              class="github-link text-decoration-none"
+              target="_blank">
+              <i class="pi pi-github" />
+              <span class="font-italic font-weight-bold">
+                See it on Github
+              </span>
+            </a>
+          </div>
+        </div>
+      </template>
+    </card>
     <v-pagination
       v-model="currentPage"
       :length="totalPages"

4141dd27a9e990a144ac21f4905c6e12b52a282e
Gabriel Mioni <gabriel.mioni@gmail.com>
2023-12-12 19:50:30 -0800

    Added FlexRow.vue

---
+++ frontend/src/components/flex/FlexRow.vue
@@ -0,0 +1,18 @@
+<template>
+  <div :class="gridClasses">
+    <slot />
+  </div>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+
+const props = defineProps({
+  noGutters: Boolean
+})
+
+const gridClasses = computed(() => ({
+  grid: true,
+  'grid-nogutter': props.noGutters
+}))
+</script>

5e1ac0b08ba8545e542e226aad4912063bde4ebe
Gabriel Mioni <gabriel.mioni@gmail.com>
2023-12-13 19:39:14 -0800

    Added FlexColumn

---
+++ frontend/src/components/flex/FlexColumn.vue
@@ -0,0 +1,64 @@
+<template>
+  <div
+    class="flex-column"
+    :class="classes">
+    <slot />
+  </div>
+</template>
+
+<script>
+
+const colPropValidator = (val) => parseInt(val) > 0 && val <= 12
+
+export default {
+  name: 'FlexColumn',
+  props: {
+    cols: {
+      type: [String, Number],
+      required: false,
+      default: null,
+      validator: colPropValidator
+    },
+    sm: {
+      type: [String, Number],
+      required: false,
+      default: null,
+      validator: colPropValidator
+    },
+    md: {
+      type: [String, Number],
+      required: false,
+      default: null,
+      validator: colPropValidator
+    },
+    lg: {
+      type: [String, Number],
+      required: false,
+      default: null,
+      validator: colPropValidator
+    },
+    xl: {
+      type: [String, Number],
+      required: false,
+      default: null,
+      validator: colPropValidator
+    }
+  },
+  computed: {
+    classes () {
+      return {
+        'col': !this.cols,
+        [`col-${this.cols}`]: this.cols,
+        [`sm:col-${this.sm}`]: this.sm,
+        [`md:col-${this.md}`]: this.md,
+        [`lg:col-${this.lg}`]: this.lg,
+        [`xl:col-${this.xl}`]: this.xl,
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>

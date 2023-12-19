0ce2e1ef7726c8145abdbc8c12e9e3f6db71b97d
Gabriel Mioni <gabriel.mioni@gmail.com>
2023-12-18 18:35:59 -0800

    In FlexColumn, added computed properties associated with differen col props

--- frontend/src/components/flex/FlexColumn.vue
+++ frontend/src/components/flex/FlexColumn.vue
@@ -51,15 +51,46 @@ export default {
     }
   },
   computed: {
+    colsVal () {
+      return this.setColumnValue(this.col)
+    },
+    smVal () {
+      return this.setColumnValue(this.sm)
+    },
+    mdVal () {
+      return this.setColumnValue(this.md)
+    },
+    lgVal () {
+      return this.setColumnValue(this.lg)
+    },
+    xlVal () {
+      return this.setColumnValue(this.xl)
+    },
     classes () {
       return {
         'col': !this.cols,
-        [`col-${this.cols}`]: this.cols,
-        [`sm:col-${this.sm}`]: this.sm,
-        [`md:col-${this.md}`]: this.md,
-        [`lg:col-${this.lg}`]: this.lg,
-        [`xl:col-${this.xl}`]: this.xl,
+        [`col-${this.colsVal}`]: this.colsVal,
+        [`sm:col-${this.smVal}`]: this.smVal,
+        [`md:col-${this.mdVal}`]: this.mdVal,
+        [`lg:col-${this.lgVal}`]: this.lgVal,
+        [`xl:col-${this.xlVal}`]: this.xlVal,
       }
+    },
+  },
+  methods: {
+    setColumnValue (val) {
+      if (!val) {
+        return null
+      }
+      const columnValue = parseInt(val)
+
+      if (columnValue > 12) {
+        return 12
+      }
+      if (columnValue < 1) {
+        return 1
+      }
+      return columnValue
     }
   }
 }

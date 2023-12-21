9578151e6adcade4804976d34e76b3c80bb23d76
Gabriel Mioni <gabriel.mioni@gmail.com>
2023-12-20 18:56:00 -0800

    Added BaseButton

---
+++ frontend/src/components/BaseButton.vue
@@ -0,0 +1,26 @@
+<template>
+  <button class="p-button base-button">
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'BaseButton'
+}
+</script>
+
+<style lang="scss" scoped>
+$white: #ffffff;
+.base-button {
+  background-color: rgba($white, 0.1);
+  padding: .5rem 1rem;
+  font-size: 14px;
+  letter-spacing: 1.25px;
+  &.p-button {
+    &:hover {
+      background-color: rgba($white, 0.2);
+    }
+  }
+}
+</style>

22583e50ca3a898bec3737554e662e4352e64f83
Gabriel Mioni <gabriel.mioni@gmail.com>
2023-12-20 18:47:23 -0800

    Refactored ToolBar to use PrimeVue Toolbar instead of Vuetify v-toolbar

--- frontend/src/views/Public/components/ToolBar.vue
+++ frontend/src/views/Public/components/ToolBar.vue
@@ -1,21 +1,29 @@
 <template>
-  <v-toolbar
-    app
-    color="#47c6c2"
-    class="text-white">
-    <v-toolbar-title>
-      Gabriel Mioni
-    </v-toolbar-title>
-    <v-spacer />
-    <v-btn
-      text
-      to="/">
-      Projects
-    </v-btn>
-    <v-btn
-      text
-      to="/about">
-      Contact
-    </v-btn>
-  </v-toolbar>
+  <toolbar class="tool-bar py-0">
+    <template #start>
+      <span class="tool-bar__title">
+        <slot name="start" />
+      </span>
+    </template>
+    <template #end>
+      <slot name="end" />
+    </template>
+  </toolbar>
 </template>
+
+<script>
+export default {
+  name: 'ToolBarNew'
+}
+</script>
+
+<style lang="scss" scoped>
+.tool-bar {
+  background-color: #47c6c2;
+  border-radius: 0;
+  color: #ffffff;
+  &__title {
+    font-size: 1.25rem;
+  }
+}
+</style>

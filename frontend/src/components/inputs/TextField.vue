e69ffac71901c3c380c54e3cded4133384d8af60
Gabriel Mioni <gabriel.mioni@gmail.com>
2024-01-04 19:44:09 -0800

    In TextField, added functionality for appending/prepending icons

--- frontend/src/components/inputs/TextField.vue
+++ frontend/src/components/inputs/TextField.vue
@@ -21,20 +21,34 @@ const props = defineProps({
   },
   small: {
     type: Boolean,
-    required: false
+    required: false,
+    default: false
   },
   medium: {
     type: Boolean,
-    required: false
+    required: false,
+    default: false
   },
   large: {
     type: Boolean,
-    required: false
+    required: false,
+    default: false
+  },
+  appendIcon: {
+    type: String,
+    required: false,
+    default: null
+  },
+  prependIcon: {
+    type: String,
+    required: false,
+    default: null
   }
 })
 
 const emit = defineEmits(['update:modelValue'])
 
+// Computed
 const textValue = computed({
   get: () => props.modelValue,
   set: (val) => emit('update:modelValue', val)
@@ -44,6 +58,16 @@ const classes = computed(() => {
   return {
     'gap-2': props.helpText
   }
+})
+
+const iconClass = computed( () => {
+  if (props.prependIcon) {
+    return 'p-input-icon-left'
+  }
+  if (props.appendIcon) {
+    return 'p-input-icon-right'
+  }
+  return null
 })
 
 const size = computed(() => {
@@ -59,9 +83,19 @@ const size = computed(() => {
 
 <template>
   <div
-    class="flex flex-column"
+    class="flex flex-column text-field"
     :class="classes">
+    <span
+      v-if="iconClass"
+      :class="iconClass"
+      class="text-field__icon-parent">
+      <i :class="`pi ${ prependIcon || appendIcon }`" />
+      <input-text
+        v-model="textValue"
+        :size="size" />
+    </span>
     <input-text
+      v-else
       v-model="textValue"
       :size="size" />
     <small
@@ -71,3 +105,12 @@ const size = computed(() => {
     </small>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.text-field {
+  &__icon-parent {
+    display: inherit;
+    flex-direction: inherit;
+  }
+}
+</style>

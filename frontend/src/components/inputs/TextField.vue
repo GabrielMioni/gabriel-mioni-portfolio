5b9d7ff5c98056966d093dc9e837f599df3b64fa
Gabriel Mioni <gabriel.mioni@gmail.com>
2024-01-04 18:36:04 -0800

    Added skeleton component TextField

---
+++ frontend/src/components/inputs/TextField.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { computed } from 'vue'
+
+const props = defineProps({
+  modelValue: {
+    type: String,
+    required: true
+  }
+})
+
+const emit = defineEmits(['update:modelValue'])
+
+const textValue = computed({
+  get: () => props.modelValue,
+  set: (val) => emit('update:modelValue', val)
+})
+
+</script>
+
+<template>
+  <input-text
+    v-model="textValue"
+    size="normal" />
+</template>

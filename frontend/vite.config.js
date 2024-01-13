5a6e2a28b2ed99236ae13cdf197c192c0535e44d
Gabriel Mioni <gabriel.mioni@gmail.com>
2024-01-12 18:12:15 -0800

    Added define object in vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,6 +11,11 @@ const __dirname = path.dirname(__filename)
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), graphql()],
+  define: {
+    __VUE_OPTIONS_API__: true,
+    __VUE_PROD_DEVTOOLS__: false,
+    __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: true
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')

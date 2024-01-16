7298445c1aba832e7708a9d539afae16e27b019d
Gabriel Mioni <gabriel.mioni@gmail.com>
2024-01-15 19:05:04 -0800

    TextArea is responsive to errors, displays label, help message

--- frontend/src/components/inputs/TextArea.vue
+++ frontend/src/components/inputs/TextArea.vue
@@ -1,7 +1,7 @@
 <script setup>
-import { fieldProps } from '@/components/inputs/field-props.js'
 import { useField } from 'vee-validate'
 import { computed } from 'vue'
+import { fieldProps } from '@/components/inputs/field-props.js'
 
 const props = defineProps({
   ...fieldProps,
@@ -24,14 +24,42 @@ const textValue = computed({
   }
 })
 
+const hasContent = computed(() => {
+  if (!fieldValue.value) {
+    return false
+  }
+  return fieldValue.value.trim().length > 0
+})
+
 </script>
 
 <template>
-  <textarea
-    v-model="textValue"
-    class="text-area p-inputtext"
-    :rows="rows" />
-  <span>{{ errorMessage }}</span>
+  <span :class="{ 'p-float-label' : floatLabel }">
+    <label v-if="!floatLabel && label">
+      Username
+    </label>
+    <textarea
+      v-model="textValue"
+      class="p-inputtextarea p-inputtext p-component text-area__input"
+      :class="{
+        'p-filled': hasContent,
+        'p-invalid': errorMessage
+      }"
+      :rows="rows" />
+    <label v-if="floatLabel && label">
+      Username
+    </label>
+  </span>
+  <div class="flex flex-column">
+    <small
+      v-if="errorMessage && !hideDetails"
+      class="p-error">
+      {{ errorMessage }}
+    </small>
+    <small v-if="helpText">
+      {{ helpText }}
+    </small>
+  </div>
 </template>
 
 <script>
@@ -41,7 +69,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.text-area {
+.text-area__input {
   width: 100%
 }
 </style>

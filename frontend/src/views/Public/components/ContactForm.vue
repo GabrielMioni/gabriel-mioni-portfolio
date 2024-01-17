6dcf60efbe1a157125b474b2a89e3d9a8eadb4ee
Gabriel Mioni <gabriel.mioni@gmail.com>
2024-01-16 18:26:51 -0800

    Removed all vuetify related components from ContactForm

--- frontend/src/views/Public/components/ContactForm.vue
+++ frontend/src/views/Public/components/ContactForm.vue
@@ -2,60 +2,80 @@
 import { ref } from 'vue'
 import { makeRequiredRule } from '@/rules/index.js'
 
+import BaseForm from '@/components/inputs/BaseForm.vue'
+import TextField from '@/components/inputs/TextField/TextField.vue'
+import TextArea from '@/components/inputs/TextArea.vue'
+
+import FlexContainer from '@/components/flex/FlexContainer.vue'
+import FlexRow from '@/components/flex/FlexRow.vue'
+import FlexColumn from '@/components/flex/FlexColumn.vue'
+import BaseButton from '@/components/BaseButton.vue'
+
 const name = ref('')
 const email = ref('')
 const message = ref('')
 const formIsValid = ref(false)
 
-const nameRules = makeRequiredRule('Name')
-const emailRules = makeRequiredRule('Email')
-const messageRules = makeRequiredRule('Message')
+const nameRule = makeRequiredRule('Name')
+const emailRule = makeRequiredRule('Email')
+const messageRule = makeRequiredRule('Message')
 </script>
 
 <template>
-  <v-form
+  <base-form
     v-model="formIsValid"
     class="contact-form">
-    <v-card
-      flat
-      class="contact-form__card">
-      <v-card-text class="pb-1">
-        <v-text-field
-          v-model="name"
-          class="mb-3"
-          hide-details
-          label="Name"
-          required
-          :rules="[nameRules]"
-          variant="solo-filled" />
-        <v-text-field
-          v-model="email"
-          class="mb-3"
-          hide-details
-          label="Email"
-          required
-          :rules="[emailRules]"
-          variant="solo-filled"
-          type="email" />
-        <v-textarea
-          v-model="message"
-          label="Message"
-          variant="solo-filled"
-          :rules="[messageRules]"
-          hide-details />
-      </v-card-text>
-      <v-card-actions>
-        <v-spacer />
-        <v-btn
-          class="text-none text-subtitle-1 mr-2"
-          color="#47c6c2"
-          variant="flat"
-          :disabled="!formIsValid">
-          Submit
-        </v-btn>
-      </v-card-actions>
-    </v-card>
-  </v-form>
+    <card>
+      <template #content>
+        <flex-container fluid>
+          <flex-row>
+            <flex-column>
+              <text-field
+                v-model="name"
+                label="Name"
+                field-name="name"
+                float-label
+                small
+                :rules="[nameRule]" />
+            </flex-column>
+          </flex-row>
+          <flex-row>
+            <flex-column>
+              <text-field
+                v-model="email"
+                label="Email"
+                field-name="email"
+                float-label
+                small
+                :rules="[emailRule]" />
+            </flex-column>
+          </flex-row>
+          <flex-row>
+            <flex-column>
+              <text-area
+                v-model="message"
+                label="Message"
+                field-name="message"
+                float-label
+                :rules="[messageRule]" />
+            </flex-column>
+          </flex-row>
+        </flex-container>
+      </template>
+      <template #footer>
+        <flex-container fluid>
+          <flex-row>
+            <flex-column />
+            <flex-column :cols="1">
+              <base-button :disabled="!formIsValid">
+                Submit
+              </base-button>
+            </flex-column>
+          </flex-row>
+        </flex-container>
+      </template>
+    </card>
+  </base-form>
 </template>
 
 <style lang="scss" scoped>

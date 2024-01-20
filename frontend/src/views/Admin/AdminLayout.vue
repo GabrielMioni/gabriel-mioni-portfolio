e1034be18389f2cf391fc4aaafb15475f31863fd
Gabriel Mioni <gabriel.mioni@gmail.com>
2024-01-19 19:35:56 -0800

    Removed Vuetify from AdminLayout

--- frontend/src/views/Admin/AdminLayout.vue
+++ frontend/src/views/Admin/AdminLayout.vue
@@ -1,17 +1,30 @@
+<script setup>
+import FlexContainer from '@/components/flex/FlexContainer.vue'
+import FlexColumn from '@/components/flex/FlexColumn.vue'
+import FlexRow from '@/components/flex/FlexRow.vue'
+
+</script>
+
 <template>
   <div>
     <main>
       <router-view v-slot="{ Component }">
-        <v-container fluid>
-          <v-row>
-            <v-col :cols="12">
+        <flex-container fluid>
+          <flex-row>
+            <flex-column :cols="12">
               <transition name="fade">
                 <component :is="Component" />
               </transition>
-            </v-col>
-          </v-row>
-        </v-container>
+            </flex-column>
+          </flex-row>
+        </flex-container>
       </router-view>
     </main>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'AdminLayout'
+}
+</script>

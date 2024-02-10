e41c19865b57657ca75e2cdbf292dba45fd8dc30
Gabriel Mioni <gabriel.mioni@gmail.com>
2024-02-09 21:47:36 -0800

    Added PopupMenu

---
+++ frontend/src/components/PopupMenu.vue
@@ -0,0 +1,43 @@
+<script setup>
+import { ref } from 'vue'
+
+const menu = ref()
+
+defineProps({
+  items: {
+    type: Array,
+    required: true
+  }
+})
+
+const toggle = (event) => {
+  menu.value.toggle(event)
+}
+
+
+</script>
+
+<template>
+  <div class="card flex justify-content-center">
+    <Button
+      type="button"
+      icon="pi pi-ellipsis-v"
+      aria-haspopup="true"
+      aria-controls="overlay_menu"
+      @click="toggle" />
+    <Menu
+      ref="menu"
+      :model="items"
+      :popup="true" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PopupMenu'
+}
+</script>
+
+<style scoped>
+
+</style>

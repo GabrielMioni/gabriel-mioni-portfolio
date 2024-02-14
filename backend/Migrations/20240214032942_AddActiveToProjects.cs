3305d06988418762eb43fa3daa3385a1a0111408
Gabriel Mioni <gabriel.mioni@gmail.com>
2024-02-13 19:40:48 -0800

    Added AddActiveToProjects migration

---
+++ backend/Migrations/20240214032942_AddActiveToProjects.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    public partial class AddActiveToProjects : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Active",
+                table: "Projects",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Active",
+                table: "Projects");
+        }
+    }
+}

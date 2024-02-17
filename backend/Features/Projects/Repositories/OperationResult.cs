73b1eb05c554ec21f863993c4aba221d6e5e25ad
Gabriel Mioni <gabriel.mioni@gmail.com>
2024-02-17 12:08:27 -0800

    Added OperationResult

---
+++ backend/Features/Projects/Repositories/OperationResult.cs
@@ -0,0 +1,19 @@
+﻿namespace backend.Features.Projects.Repositories
+{
+    public class OperationResult
+    {
+        public bool Succeeded { get; set; }
+        public string Message { get; set; }
+
+        public static OperationResult Fail(string message)
+        {
+            return new OperationResult { Succeeded = false, Message = message };
+        }
+
+        public static OperationResult Success(string? message = null)
+        {
+            var displayMessage = message != null ? message : "Operation succeeded.";
+            return new OperationResult { Succeeded = true, Message = displayMessage };
+        }
+    }
+}

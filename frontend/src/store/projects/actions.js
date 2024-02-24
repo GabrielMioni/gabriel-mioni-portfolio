714c30ab73023f9277a56ac0460b973432d9c698
Gabriel Mioni <gabriel.mioni@gmail.com>
2024-02-23 20:35:56 -0800

    In actions.js, added setProjectActive

--- frontend/src/store/projects/actions.js
+++ frontend/src/store/projects/actions.js
@@ -12,6 +12,24 @@ export const actions = {
     this.PROJECTS = projects
     this.PROJECTS_COUNT = count
   },
+  setProjectActive ({ id, setActive }) {
+    const projectIndex = this.PROJECTS.findIndex(p => p.id === id)
+    if (projectIndex < 0) {
+      console.warn(`Unable to update active value using id of ${id}`)
+      return
+    }
+    const updateProject = {
+      ...this.PROJECTS[projectIndex],
+      active: setActive
+    }
+    this.$patch({
+      PROJECTS: [
+        ...this.PROJECTS.slice(0, projectIndex),
+        updateProject,
+        ...this.PROJECTS.slice(projectIndex + 1)
+      ]
+    })
+  },
   setProjectsLoading (value) {
     this.PROJECTS_LOADING = value
   }

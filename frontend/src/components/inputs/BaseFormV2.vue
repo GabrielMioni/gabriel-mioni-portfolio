4d5152f9ab8c103d3c1405d20e4ca718cd349b41
Gabriel Mioni <gabriel.mioni@gmail.com>
2024-03-13 20:07:09 -0700

    Added BaseFormV2

---
+++ frontend/src/components/inputs/BaseFormV2.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { Form  } from 'vee-validate'
+</script>
+
+<template>
+  <Form v-slot="{ meta }">
+    <slot
+      :is-valid="meta?.valid || false" />
+  </Form>
+</template>
+
+<script>
+export default {
+  name: 'BaseFormV2'
+}
+</script>
+
+
+<style scoped lang="scss">
+
+</style>

7c198b4772fc3bec93775e69cd9e67ae204b4f6f
Gabriel Mioni <gabriel.mioni@gmail.com>
2024-03-13 20:47:24 -0700

    TextFieldV2 uses TextFieldRender

--- frontend/src/components/TextFieldV2.vue
+++ frontend/src/components/TextFieldV2.vue
@@ -1,25 +1,40 @@
 <script setup>
 import { computed } from 'vue'
+import { fieldProps } from '@/components/inputs/field-props.js'
 import { ErrorMessage, Field } from 'vee-validate'
+import TextFieldRender from '@/components/inputs/TextField/TextFieldRender.vue'
 
 const props = defineProps({
-  modelValue: {
-    type: String,
-    required: true
+  ...fieldProps,
+  small: {
+    type: Boolean,
+    required: false,
+    default: false
   },
-  fieldName: {
+  medium: {
+    type: Boolean,
+    required: false,
+    default: false
+  },
+  large: {
+    type: Boolean,
+    required: false,
+    default: false
+  },
+  appendIcon: {
     type: String,
-    required: true
+    required: false,
+    default: null
+  },
+  prependIcon: {
+    type: String,
+    required: false,
+    default: null
   },
   type: {
     type: String,
     required: false,
     default: 'text'
-  },
-  rules: {
-    type: Array,
-    required: false,
-    default: () => []
   }
 })
 
@@ -30,20 +45,42 @@ const fieldValue = computed({
   set: (val) => emit('update:modelValue', val)
 })
 
+const fieldNameDisplay = props.fieldName.toString().toLowerCase()
+
 </script>
 
 <template>
-  <Field
-    v-slot="{ field, errors, meta }"
-    :name="fieldName"
-    :type="type"
-    :rules="rules">
-    <input-text
-      v-model="fieldValue"
-      v-bind="field"
-      :class="{ 'p-invalid': errors.length > 0 && meta.touched }" />
-  </Field>
-  <ErrorMessage :name="fieldName" />
+  <text-field-render
+    :append-icon="appendIcon"
+    :hide-details="hideDetails"
+    :prepend-icon="prependIcon"
+    :float-label="floatLabel"
+    :help-text="helpText"
+    class="text-field">
+    <template #label>
+      <label
+        v-if="label"
+        :for="fieldNameDisplay"
+        style="border-radius: 5px">
+        {{ label }}
+      </label>
+    </template>
+    <Field
+      v-slot="{ field, errors, meta }"
+      :name="fieldName"
+      :type="type"
+      :rules="rules">
+      <input-text
+        v-model="fieldValue"
+        v-bind="field"
+        :class="{ 'p-invalid': errors.length > 0 && meta.touched }" />
+    </Field>
+    <template #error>
+      <small class="p-error">
+        <ErrorMessage :name="fieldName" />
+      </small>
+    </template>
+  </text-field-render>
 </template>
 
 <style scoped lang="scss">

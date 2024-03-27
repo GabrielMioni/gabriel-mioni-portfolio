f3f74853c14024a517cc3ebc02aa3350a263b3df
Gabriel Mioni <gabriel.mioni@gmail.com>
2024-03-26 19:38:46 -0700

    Added ToastMessage

---
+++ frontend/src/components/ToastMessage.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { useToast } from 'primevue/usetoast'
+import eventBus from '@/services/EventBus.js'
+
+const toast = useToast()
+
+eventBus.$on('toast', ({ severity, summary, message, life = 3000 }) => {
+  toast.add({ severity, summary, detail: message, life })
+})
+
+eventBus.$off('toast')
+
+</script>
+
+<template>
+  <Toast class="toast-message" />
+</template>
+
+<script>
+export default {
+  name: 'ToastMessage'
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>

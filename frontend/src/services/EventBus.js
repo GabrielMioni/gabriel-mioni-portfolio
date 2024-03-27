c5bd99791be9b6311af70cc60d6561cfc5e255ce
Gabriel Mioni <gabriel.mioni@gmail.com>
2024-03-26 19:38:32 -0700

    Added EventBus.js

---
+++ frontend/src/services/EventBus.js
@@ -0,0 +1,7 @@
+import emitter from 'tiny-emitter/instance'
+
+export default {
+  $on: (...args) => emitter.on(...args),
+  $off: (...args) => emitter.off(...args),
+  $emit: (...args) => emitter.emit(...args)
+}

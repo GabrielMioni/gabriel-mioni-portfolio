b2c83a623d55baec9036ca84cabf1f5e228fd673
Gabriel Mioni <gabriel.mioni@gmail.com>
2024-03-28 18:12:31 -0700

    Removed files/components related to earlier versions of form inputs

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,11 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import AboutView from '@/views/Public/AboutView.vue'
-// import AddProject from '@/views/Admin/AddProject.vue'
 import AdminLayout from '@/views/Admin/AdminLayout.vue'
 import ProjectsView from '@/views/Public/ProjectsView.vue'
 import PublicLayout from '@/views/Public/PublicLayout.vue'
 import ProjectsTable from '@/views/Admin/ProjectsTable/ProjectsTable.vue'
-import PrimeVue from '@/views/Public/PrimeVue.vue'
 import ValidationForm from '@/components/ValidationForm.vue'
 
 const routes = [
@@ -20,10 +18,6 @@ const routes = [
       {
         path: '/about',
         component: AboutView
-      },
-      {
-        path: '/prime-vue',
-        component: PrimeVue
       }
     ]
   },

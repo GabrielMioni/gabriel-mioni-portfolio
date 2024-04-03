8b6ed547246bd0c9e7d792eeb65fa00f3a426e5f
Gabriel Mioni <gabriel.mioni@gmail.com>
2024-04-02 18:30:15 -0700

    Added uploadFile in api/index.js

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -3,3 +3,9 @@ import axios from '@/api/axios'
 export const fetchWeatherForecast = async () => {
   return axios.get('/WeatherForecast')
 }
+
+export const uploadFile = async (file) => {
+  const formData = new FormData()
+  formData.append('file', file)
+  return axios.post('/api/Files', formData)
+}

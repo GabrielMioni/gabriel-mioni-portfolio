3d89d6491da2c087d86fc472a0e5fd2dfb6c4a38
Gabriel Mioni <gabriel.mioni@gmail.com>
2024-05-09 20:03:52 -0700

    Refactored ProjectResult to use ProjectDto for Nodes instead of Project

--- backend/Features/Projects/GraphQL/ProjectResult.cs
+++ backend/Features/Projects/GraphQL/ProjectResult.cs
@@ -4,7 +4,7 @@ namespace backend.Features.Projects.GraphQL
 {
     public class ProjectResult
     {
-        public List<Project> Nodes { get; set; }
+        public List<ProjectDto> Nodes { get; set; }
         public List<string> Errors { get; set; }
         public int Count { get; set; }
     }

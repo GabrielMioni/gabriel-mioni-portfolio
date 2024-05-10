dd1fa5513008bbadcc7817976e6f5183d62d5718
Gabriel Mioni <gabriel.mioni@gmail.com>
2024-05-09 19:52:43 -0700

    Added ProjectDto

---
+++ backend/Features/Projects/Models/ProjectDto.cs
@@ -0,0 +1,14 @@
+﻿using backend.Features.Aws.Services;
+
+namespace backend.Features.Projects.Models
+{
+    public class ProjectDto
+    {        
+        public string Id { get; set; }
+        public bool Active { get; set; }
+        public string Description { get; set; }
+        public string? Git { get; set; }
+        public string? ImageUrl { get; set; }
+        public string Name { get; set; }
+    }
+}

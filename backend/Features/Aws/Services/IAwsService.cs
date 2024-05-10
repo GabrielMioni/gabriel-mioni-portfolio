4f03c8b1484523b061b5f12cc90d2865da0b09ef
Gabriel Mioni <gabriel.mioni@gmail.com>
2024-05-09 19:46:58 -0700

    In IAwsService, added GetFileUrlByKey method

--- backend/Features/Aws/Services/IAwsService.cs
+++ backend/Features/Aws/Services/IAwsService.cs
@@ -8,6 +8,7 @@ namespace backend.Features.Aws.Services
         public Task<FileUploadResultDto> UploadFileAsync(IFormFile file, string? prefix);
         public Task<IEnumerable<S3ObjectDto>> GetAllFilesAsync();
         public Task<FileResultDto> GetFileByKeyAsync(string key);
+        public string GetFileUrlByKey(string key);
         public Task DeleteFileByKeyAsync(string key);
     }
 }

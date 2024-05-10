066801766f454b606df2b5377b38257097b2b725
Gabriel Mioni <gabriel.mioni@gmail.com>
2024-05-09 19:51:26 -0700

    Added IImageRepository and concerete implementation

---
+++ backend/Features/Projects/Repositories/IImageRepository.cs
@@ -0,0 +1,10 @@
+﻿using backend.Data;
+using backend.Features.Projects.Models;
+
+namespace backend.Features.Projects.Repositories
+{
+    public interface IImageRepository
+    {
+        Task<Image> AddImageAsync(Image newImage);
+    }
+}
---
+++ backend/Features/Projects/Repositories/ImageRepository.cs
@@ -0,0 +1,21 @@
+﻿using backend.Data;
+using backend.Features.Projects.Models;
+
+namespace backend.Features.Projects.Repositories
+{
+    public class ImageRepository : IImageRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public ImageRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Image> AddImageAsync(Image newImage)
+        {
+            _context.Images.Add(newImage);
+            await _context.SaveChangesAsync();
+            return newImage;
+        }
+    }
+}

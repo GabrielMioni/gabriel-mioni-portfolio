24a05e746851292ecf179272ea4a38413a936598
Gabriel Mioni <gabriel.mioni@gmail.com>
2024-05-09 19:42:56 -0700

    In IProjectsRepository, added GetProjectsAsync method

--- backend/Features/Projects/Repositories/IProjectsRepository.cs
+++ backend/Features/Projects/Repositories/IProjectsRepository.cs
@@ -5,7 +5,7 @@ namespace backend.Features.Projects.Repositories
     public interface IProjectsRepository
     {
         Task<Project> AddProjectAsync(Project newProject);
-        Task<List<Project>> GetProjectsAsync(int skip, int take, bool IncludeInactive = false);
+        Task<List<ProjectDto>> GetProjectsAsync(int skip, int take, bool IncludeInactive = false);
         Task<int> GetProjectCountAsync();
         Task<OperationResult> SetProjectActiveAsync(string id, Boolean value);
         Task<OperationResult> EditProjectAsync(string id, string name, string description, string? git = "");

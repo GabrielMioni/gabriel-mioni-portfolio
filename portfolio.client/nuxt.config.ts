a9282f871a813423bd37f72f2acf0f0b0d751bd0
Gabriel Mioni <gabriel.mioni@gmail.com>
2026-02-01 14:07:32 -0800

    Added proxy for api in nuxt.config.ts

--- portfolio.client/nuxt.config.ts
+++ portfolio.client/nuxt.config.ts
@@ -2,5 +2,17 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint']
+  modules: ['@nuxt/eslint'],
+  runtimeConfig: {
+    public: {
+      apiBase: '/api'
+    }
+  },
+  nitro: {
+    routeRules: {
+      '/api/**': {
+        proxy: 'http://127.0.0.1:5217/api/**'
+      }
+    }
+  }
 })

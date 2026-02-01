c811aa5c8e8465132e3564aaffd515ac18157ff8
Gabriel Mioni <gabriel.mioni@gmail.com>
2026-02-01 14:16:48 -0800

    nuxt.config.ts uses localhost in routeRules

--- portfolio.client/nuxt.config.ts
+++ portfolio.client/nuxt.config.ts
@@ -11,7 +11,7 @@ export default defineNuxtConfig({
   nitro: {
     routeRules: {
       '/api/**': {
-        proxy: 'http://127.0.0.1:5217/api/**'
+        proxy: 'http://localhost:5217/api/**'
       }
     }
   }

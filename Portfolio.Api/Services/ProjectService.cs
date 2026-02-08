3ff83bf8328209126c6ac2ed897ad1e9774c257b
Gabriel Mioni <gabriel.mioni@gmail.com>
2026-02-08 11:21:26 -0800

    Added ProjectService.cs

---
+++ Portfolio.Api/Services/ProjectService.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Portfolio.Api.Data;
+using Portfolio.Api.Domain.Projects;
+
+namespace Portfolio.Api.Services
+{
+    public class ProjectService
+    {
+        private readonly IDbContextFactory<AppDbContext> _dbFactory;
+
+        public ProjectService(IDbContextFactory<AppDbContext> dbFactory)
+        {
+            _dbFactory = dbFactory;
+        }
+
+        public async Task<Project?> GetByIdAsync(Guid id)
+        {
+            await using var db = await _dbFactory.CreateDbContextAsync();
+            return await db.Projects.FirstOrDefaultAsync(p => p.Id == id);
+        }
+    }
+}

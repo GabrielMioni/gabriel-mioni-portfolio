acca400f9f268e7c0649b71636d7fe7d13874785
Gabriel Mioni <gabriel.mioni@gmail.com>
2026-02-10 19:56:26 -0800

    In codegen.ts, set schema to localhost:5217/graphql, config.useTypeImports is true, added generation for schema.graphql

--- portfolio.client/codegen.ts
+++ portfolio.client/codegen.ts
@@ -1,18 +1,22 @@
 import type { CodegenConfig } from '@graphql-codegen/cli'
 
 const config: CodegenConfig = {
-  schema: 'https://localhost:7022/graphql',
+  schema: 'http://localhost:5217/graphql',
   documents: ['app/graphql/**/*.gql'],
   generates: {
     './app/generated/': {
       preset: 'client',
       config: {
+        useTypeImports: true,
         scalars: {
           UUID: 'string',
           DateTime: 'string',
           DateTimeOffset: 'string'
         }
       }
+    },
+    './app/generated/schema.graphql': {
+      plugins: ['schema-ast']
     }
   }
 }

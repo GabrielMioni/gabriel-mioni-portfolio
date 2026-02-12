514ec41587933d95b75918abae624df988ca232b
Gabriel Mioni <gabriel.mioni@gmail.com>
2026-02-11 18:40:58 -0800

    Corrected urql.ts plugin so that the client is pased to vue. Added projects page with simple query to prove urql is working

---
+++ portfolio.client/app/pages/projects/index.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { useQuery } from '@urql/vue'
+import { GetProjectByIdDocument } from '~/generated/graphql'
+
+const { data, fetching, error } = useQuery({
+  query: GetProjectByIdDocument,
+  variables: {
+    id: '3b61d270-70f3-4a1f-421c-08de6781bdd2'
+  }
+})
+</script>
+
+<template>
+  <div v-if="fetching">
+    Loading...
+  </div>
+  <div v-else-if="error">
+    Error: {{ error }}
+  </div>
+  <div v-else>
+    <pre>{{ data }}</pre>
+  </div>
+</template>
+
+<style scoped>
+
+</style>

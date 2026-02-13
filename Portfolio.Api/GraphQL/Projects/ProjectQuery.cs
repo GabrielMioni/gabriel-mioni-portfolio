c909da90d617b043fb6e06802c4878c14d5ba862
Gabriel Mioni <gabriel.mioni@gmail.com>
2026-02-12 18:34:44 -0800

    Added ProjectQuery.GetProjects

--- Portfolio.Api/GraphQL/Projects/ProjectQuery.cs
+++ Portfolio.Api/GraphQL/Projects/ProjectQuery.cs
@@ -1,4 +1,5 @@
-﻿using Portfolio.Api.Domain.Projects;
+﻿using Portfolio.Api.Data;
+using Portfolio.Api.Domain.Projects;
 using Portfolio.Api.Services;
 
 namespace Portfolio.Api.GraphQL.Projects
@@ -14,6 +15,17 @@ namespace Portfolio.Api.GraphQL.Projects
         public Task<List<Project>> GetPublsihedProjects([Service] ProjectService projects, CancellationToken ct = default)
         {
             return projects.GetPublishedAsync(ct);
+        }
+
+        [UsePaging]
+        [UseSorting]
+        [UseFiltering]
+        public IQueryable<Project> GetProjects(
+            [Service] AppDbContext db,
+            [Service] ProjectService projectService,
+            bool includeUnpublished = false)
+        {
+            return projectService.QueryProjects(db, includeUnpublished);
         }
     }
 }

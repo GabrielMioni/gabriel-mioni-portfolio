ee1b4e04e9cdd4340e22d400815cae28c883023a
Gabriel Mioni <gabriel.mioni@gmail.com>
2026-02-16 20:03:38 -0800

    In useProjects composable, added computed property variables, passed to useQuery for GetProjectsDocument

--- portfolio.client/app/composables/useProjects.ts
+++ portfolio.client/app/composables/useProjects.ts
@@ -2,25 +2,29 @@ import { useQuery } from '@urql/vue'
 import {
   type Project,
   type ProjectQueryProjectsArgs,
-  SortEnumType,
+  // SortEnumType,
   GetProjectsDocument
 } from '~/generated/graphql'
 
-export const useProjects = (input: ProjectQueryProjectsArgs) => {
+export const useProjects = (input: Ref<ProjectQueryProjectsArgs>) => {
+  const variables = computed(() => ({
+    skip: input.value.skip ?? 0,
+    take: input.value.take ?? 5,
+    includeUnpublished: input.value.includeUnpublished ?? true,
+    // order: input.value.order ?? [{ createdAt: SortEnumType.Desc }],
+    order: input.value.order ?? [],
+    where: input.value.where
+  }))
+
   const {
     data,
     fetching: fetchingProjects,
     error: projectError
   } = useQuery({
     query: GetProjectsDocument,
-    variables: {
-      skip: input.skip ?? 0,
-      take: input.take ?? 5,
-      includeUnpublished: input.includeUnpublished ?? true,
-      order: input.order ?? [{ createdAt: SortEnumType.Desc }],
-      where: input.where
-    }
+    variables
   })
+
 
   const projects = computed(() =>
     (data.value?.projects?.items ?? []).filter(

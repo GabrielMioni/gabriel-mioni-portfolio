4ce4c8131c1ec574cf31d287156ddb4c96b02df2
Gabriel Mioni <gabriel.mioni@gmail.com>
2026-02-16 19:35:58 -0800

    Added BaseTable

---
+++ portfolio.client/app/components/base-components/BaseTable.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import type { Items, TableOptions } from '~/types/ui/datatable'
+
+const options = defineModel<TableOptions>('options', {
+  default: () => ({
+    page: 1,
+    itemsPerPage: 10,
+    sortBy: [],
+    groupBy: [],
+    search: ''
+  })
+})
+
+defineProps<{
+  items: NonNullable<Items>
+  itemsLength: number
+}>()
+
+</script>
+
+<template>
+  <v-data-table-server
+    v-model:options="options"
+    :items="items"
+    :items-length="itemsLength" />
+</template>

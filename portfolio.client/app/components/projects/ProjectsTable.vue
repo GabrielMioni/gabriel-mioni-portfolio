d9f4b6547a70ed1f20e53798173eab84df6eb81f
Gabriel Mioni <gabriel.mioni@gmail.com>
2026-02-16 19:36:33 -0800

    Added ProjectsTable

---
+++ portfolio.client/app/components/projects/ProjectsTable.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import type { Project } from '~/generated/graphql'
+import type { TableOptions } from '~/types/ui/datatable'
+import BaseTable from '~/components/base-components/BaseTable.vue'
+
+const options = defineModel<TableOptions>('options')
+
+defineProps<{
+  projects: Project[],
+  totalCount: number
+}>()
+
+</script>
+
+<template>
+  <BaseTable
+    v-model:options="options"
+    :items="projects"
+    :items-length="totalCount" />
+</template>

c8f7816c539e55d6ed401609c3174c808197fc9a
Gabriel Mioni <gabriel.mioni@gmail.com>
2026-02-17 19:57:50 -0800

    ProjectTable emits v-model:search. Pass appendIcon and useSearch props to BaseTable

--- portfolio.client/app/components/projects/ProjectsTable.vue
+++ portfolio.client/app/components/projects/ProjectsTable.vue
@@ -4,6 +4,7 @@ import type { TableOptions } from '~/types/ui/datatable'
 import BaseTable from '~/components/base-components/BaseTable.vue'
 
 const options = defineModel<TableOptions>('options')
+const search = defineModel<string>('search')
 
 defineProps<{
   projects: Project[],
@@ -15,6 +16,9 @@ defineProps<{
 <template>
   <BaseTable
     v-model:options="options"
+    v-model:search="search"
     :items="projects"
-    :items-length="totalCount" />
+    :items-length="totalCount"
+    append-icon="mdi-magnify"
+    use-search />
 </template>

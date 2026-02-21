d335fc7c7297d7a5a7ae99c9cb8becfc9592c785
Gabriel Mioni <gabriel.mioni@gmail.com>
2026-02-21 10:00:17 -0800

    Added BaseDialog

---
+++ portfolio.client/app/components/base-components/BaseDialog.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+const dialog = defineModel<boolean>()
+
+withDefaults(
+  defineProps<{
+    title?: string
+    divider?: boolean
+    toolbarColor?: string
+  }>(),
+  {
+    title: undefined,
+    divider: false,
+    toolbarColor: 'primary'
+  }
+)
+
+</script>
+
+<template>
+  <v-dialog
+    v-model="dialog">
+    <v-toolbar
+      :color="toolbarColor"
+      density="comfortable"
+      flat>
+      <v-toolbar-title v-if="title">
+        {{ title}}
+      </v-toolbar-title>
+      <v-spacer />
+      <v-btn
+        icon="mdi-close"
+        variant="text"
+        @click="dialog = false" />
+    </v-toolbar>
+    <v-card>
+      <v-card-text v-if="$slots.default">
+        <slot name="default" />
+      </v-card-text>
+      <v-divider v-if="divider" />
+      <v-card-actions v-if="$slots.actions">
+        <slot name="actions" />
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<style scoped>
+
+</style>

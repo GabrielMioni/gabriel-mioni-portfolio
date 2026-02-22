3570bebf8bdd3d27cf64ff88a1c4247007cccdd8
Gabriel Mioni <gabriel.mioni@gmail.com>
2026-02-22 10:39:35 -0800

    Added cacheExchange in urql.ts plugin

--- portfolio.client/app/plugins/urql.ts
+++ portfolio.client/app/plugins/urql.ts
@@ -1,11 +1,11 @@
-import urql, { createClient, fetchExchange, type ClientOptions } from '@urql/vue'
+import urql, { createClient, cacheExchange, fetchExchange, type ClientOptions } from '@urql/vue'
 
 export default defineNuxtPlugin((nuxtApp) => {
   const config = useRuntimeConfig()
 
   const clientOptions: ClientOptions = {
     url: config.public.graphQlBase, // '/graphql'
-    exchanges: [fetchExchange],
+    exchanges: [cacheExchange, fetchExchange],
     fetchOptions: {
       credentials: 'include'
     }

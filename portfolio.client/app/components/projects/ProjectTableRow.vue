bec3a1c8850b835c962ca8f02c17eb40b280c3e6
Gabriel Mioni <gabriel.mioni@gmail.com>
2026-02-23 18:56:53 -0800

    In ProjectTableRow.vue, format td text with computed proeprty view

--- portfolio.client/app/components/projects/ProjectTableRow.vue
+++ portfolio.client/app/components/projects/ProjectTableRow.vue
@@ -1,21 +1,36 @@
 <script setup lang="ts">
 import type { Project } from '~/generated/graphql'
+import { formatDate, formatTextWithEllipsis } from '~/utils/formatters'
 
-defineProps<{
+const props = defineProps<{
   project: Project
 }>()
+
+const dash = '-'
+
+const maybeText = (val?: string | null, ellipsis = true) => {
+  if (!val) return dash
+  return ellipsis ? formatTextWithEllipsis(val) : val
+}
+
+const view = computed(() => ({
+  title: props.project.title,
+  summary: maybeText(props.project.summary),
+  body: maybeText(props.project.body),
+  createdAt: props.project.createdAt ? formatDate(props.project.createdAt) : dash
+}))
 
 </script>
 
 <template>
   <tr>
-    <td>{{ project.title }}</td>
-    <td>{{ project.body }}</td>
-    <td>{{ project.publishedAt }}</td>
-    <td>{{ project.createdAt }}</td>
-    <td>{{ project.updatedAt }}</td>
-    <td>{{ project.summary }}</td>
-    <td>{{ project.status }}</td>
+    <td v-text="view.title" />
+    <td v-text="view.summary" />
+    <td v-text="view.body" />
+    <td v-text="view.createdAt" />
+    <td>
+      <ProjectStatus :project="project" />
+    </td>
     <td class="text-end">
       <ProjectMenu :project-id="project.id"/>
     </td>

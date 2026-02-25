43bf78317ea4e48fdc2a312e97f3e3a35039699c
Gabriel Mioni <gabriel.mioni@gmail.com>
2026-02-24 19:31:14 -0800

    BaseTable provides isExpanded and toggleExpanded props from item slot. Added expanded-row slot

--- portfolio.client/app/components/base-components/BaseTable.vue
+++ portfolio.client/app/components/base-components/BaseTable.vue
@@ -11,33 +11,44 @@ const options = defineModel<TableOptions>('options', {
   })
 })
 
-withDefaults(
-  defineProps<{
-    headers?: Header[] | undefined
-    items: T[]
-    itemsLength: number
-  }>(),
-  {
-    headers: undefined
-  }
-)
+const expanded = defineModel<string[]>('expanded', {
+  default: () => []
+})
 
+withDefaults(defineProps<{
+  headers?: Header[] | undefined
+  items: T[]
+  itemsLength: number
+  itemValue?: string
+}>(), {
+  headers: undefined,
+  itemValue: 'id'
+})
 </script>
 
 <template>
   <v-data-table-server
     v-model:options="options"
+    v-model:expanded="expanded"
     :headers="headers"
     :items="items"
-    :items-length="itemsLength">
+    :items-length="itemsLength"
+    :item-value="itemValue">
     <template
       v-if="$slots.top"
       #top>
       <slot name="top" />
     </template>
-    <template #item="{ item }">
+    <template #item="{ item, internalItem, isExpanded, toggleExpand }">
       <slot
         name="item"
+        :item="item as T"
+        :is-expanded="isExpanded(internalItem)"
+        :toggle-expand="() => toggleExpand(internalItem)" />
+    </template>
+    <template #expanded-row="{ item }">
+      <slot
+        name="expanded-row"
         :item="item as T" />
     </template>
   </v-data-table-server>

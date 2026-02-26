373daa647bd2e85e87d906d793d84259aa0b7eb8
Gabriel Mioni <gabriel.mioni@gmail.com>
2026-02-25 21:11:36 -0800

    nuxt.config.ts includes css property

--- portfolio.client/nuxt.config.ts
+++ portfolio.client/nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
+  css: ['~/assets/scss/main.scss'],
   devtools: { enabled: true },
   devServer: {
     port: 3000

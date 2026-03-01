3fa5a4ead2fa26b10a17aa7eb00bdd5c422801ed
Gabriel Mioni <gabriel.mioni@gmail.com>
2026-03-01 10:57:36 -0800

    Added RequestProjectImageUploadsInput.cs

---
+++ Portfolio.Api/GraphQL/Projects/Inputs/RequestProjectImageUploadsInput.cs
@@ -0,0 +1,19 @@
+﻿namespace Portfolio.Api.GraphQL.Projects.Inputs;
+
+public enum ProjectImageUploadVariant
+{
+    Full,
+    Thumb
+}
+
+public record ProjectImageUploadRequestItemInput(
+    string ImageClientId,
+    ProjectImageUploadVariant Variant,
+    string ContentType,
+    long SizeBytes
+);
+
+public record RequestProjectImageUploadsInput(
+    Guid ProjectId,
+    IReadOnlyList<ProjectImageUploadRequestItemInput> Items
+);

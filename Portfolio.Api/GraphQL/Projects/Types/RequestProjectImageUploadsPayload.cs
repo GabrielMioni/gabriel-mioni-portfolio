c0f591b072c2663e2d0029dfa182534373ac3b84
Gabriel Mioni <gabriel.mioni@gmail.com>
2026-03-01 11:00:28 -0800

    Added RequestProjectImageUploadsPayload.cs

---
+++ Portfolio.Api/GraphQL/Projects/Types/RequestProjectImageUploadsPayload.cs
@@ -0,0 +1,15 @@
+﻿namespace Portfolio.Api.GraphQL.Projects.Types;
+
+using Portfolio.Api.GraphQL.Projects.Inputs;
+
+public record ProjectImageUploadInstruction(
+    string ImageClientId,
+    ProjectImageUploadVariant Variant,
+    string Key,
+    string UploadUrl,
+    string PublicUrl
+);
+
+public record RequestProjectImageUploadsPayload(
+    IReadOnlyList<ProjectImageUploadInstruction> Items
+);

9f65701da18af2c3b8063597258e8ff3f59dbbe7
Gabriel Mioni <gabriel.mioni@gmail.com>
2026-03-02 21:15:34 -0800

    Added pages/projects/create.vue

---
+++ portfolio.client/app/pages/projects/create.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { ProjectStatus } from '~/generated/graphql'
+
+const form = reactive({
+  title: '',
+  summary: '',
+  body: '',
+  status: ProjectStatus.Draft
+})
+const isValid = ref(false)
+</script>
+
+<template>
+  <v-container>
+    <h3>Create Page</h3>
+    <ProjectForm
+      v-model:form="form"
+      v-model:is-valid="isValid" />
+  </v-container>
+</template>
+
+<style scoped>
+
+</style>

952603b0e6596930780be85517af5a557f827512
Gabriel Mioni <gabriel.mioni@gmail.com>
2026-03-09 18:29:24 -0700

    ProjectImageUploadListItem moves altText v-text-field to second line on small screens

--- portfolio.client/app/components/projects/edit/images/list/ProjectImageUploadListItem.vue
+++ portfolio.client/app/components/projects/edit/images/list/ProjectImageUploadListItem.vue
@@ -22,39 +22,44 @@ const imageFileName = computed(() => {
   <v-container
     class="pa-0 project-image-upload-list-item hover-surface"
     fluid>
-    <v-row>
+    <v-row align="center">
       <v-col
         cols="auto"
-        class="d-flex align-center justify-center">
+        class="d-flex align-center justify-center order-1">
         <v-img
           v-if="imageUrl"
           class="ma-2"
           :src="imageUrl"
-          width="50"/>
+          width="50" />
       </v-col>
-      <v-col cols="auto">
+      <v-col
+        class="order-2"
+        md="2">
         <div class="image-details fs-12">
           <div>{{ imageFileName }}</div>
           <div>{{ item.contentType }}</div>
           <div>{{ (item.sizeFull / 1024).toFixed(2) }} KB</div>
         </div>
       </v-col>
-      <v-col class="d-flex align-center">
-        <v-text-field
-          v-model="item.altText"
-          variant="filled"
-          label="Alt Text"
-          hide-details />
-      </v-col>
       <v-col
         cols="auto"
-        class="d-flex align-center">
+        class="d-flex align-center justify-end order-3 order-sm-4">
         <v-btn
           icon="mdi-close"
           class="ma-2"
           variant="text"
           color="error"
-          @click="$emit('remove', item.clientId)" />
+          @click="$emit('remove', item.clientId)"/>
+      </v-col>
+      <v-col
+        cols="12"
+        sm
+        class="order-4 order-sm-3">
+        <v-text-field
+          v-model="item.altText"
+          variant="filled"
+          label="Alt Text"
+          hide-details />
       </v-col>
     </v-row>
   </v-container>
@@ -62,7 +67,6 @@ const imageFileName = computed(() => {
 
 <style scoped>
 .image-details {
-  width: 10rem;
   overflow-wrap: break-word;
 }
 </style>

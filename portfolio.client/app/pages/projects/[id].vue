e9444db1ecc43507832641d3b435ea1df58149ef
Gabriel Mioni <gabriel.mioni@gmail.com>
2026-03-10 20:15:34 -0700

    In pages/projects/[id].vue uses useProjectMutations composable, added cancel/save buttons

--- portfolio.client/app/pages/projects/[id].vue
+++ portfolio.client/app/pages/projects/[id].vue
@@ -1,12 +1,18 @@
 <script setup lang="ts">
 import { useQuery } from '@urql/vue'
 import {
+  type EditProjectInput,
   ProjectStatus,
   GetProjectByIdDocument,
   ProjectFragmentDoc
 } from '~/generated/graphql'
 import { useFragment } from '~/generated'
 import ProjectImageUpload from '~/components/projects/edit/images/ProjectImageUpload.vue'
+
+const {
+  editing,
+  editProject
+} = useProjectMutations()
 
 const tabValues = {
   details: 'details',
@@ -42,6 +48,22 @@ const project = computed(() => {
   return ref ? useFragment(ProjectFragmentDoc, ref) : null
 })
 
+const updateInput = computed<EditProjectInput>(() => ({
+  id: id,
+  title: form.title,
+  summary: form.summary,
+  body: form.body,
+  status: form.status
+}))
+
+const submitEditProject = async () => {
+  try {
+    await editProject(updateInput.value)
+  } catch (error) {
+    console.error('Failed to save project', error)
+  }
+}
+
 watch(
   project,
   (project) => {
@@ -66,12 +88,28 @@ watch(
         size="60"
         indeterminate />
     </div>
-    <div v-else>
-      <v-tabs v-model="tab">
-        <v-tab :value="tabValues.details">Details</v-tab>
-        <v-tab :value="tabValues.images">Images</v-tab>
-      </v-tabs>
-
+    <v-card
+      v-else
+      flat>
+      <v-toolbar color="transparent">
+        <v-tabs v-model="tab">
+          <v-tab :value="tabValues.details">Details</v-tab>
+          <v-tab :value="tabValues.images">Images</v-tab>
+        </v-tabs>
+        <v-spacer />
+        <v-btn
+          text
+          class="mr-3"
+          @click="$router.back()">
+          Cancel
+        </v-btn>
+        <v-btn
+          class="bg-primary"
+          :loading="editing"
+          @click="submitEditProject">
+          Save
+        </v-btn>
+      </v-toolbar>
       <v-tabs-window v-model="tab">
         <div class="mt-3">
           <v-tabs-window-item :value="tabValues.details">
@@ -84,7 +122,7 @@ watch(
           </v-tabs-window-item>
         </div>
       </v-tabs-window>
-    </div>
+    </v-card>
   </v-container>
 </template>
 

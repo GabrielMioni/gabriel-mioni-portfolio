ddb03d02975f5937fa0751142a983a310fb4129f
Gabriel Mioni <gabriel.mioni@gmail.com>
2026-03-18 12:21:09 -0700

    In ImageUploadItem type, added fields related to persisted project images

--- portfolio.client/app/types/images/ImageUploadItem.ts
+++ portfolio.client/app/types/images/ImageUploadItem.ts
@@ -1,12 +1,16 @@
 export type ImageUploadItem = {
+  id?: string | null
   clientId: string
   contentType: string
-  fileName: string
+  fileName?: string | null
+  fullKey?: string | null
+  thumbKey?: string | null
+  sort: number
   sizeThumb: number
   sizeFull: number
   altText: string
-  thumbFile: Blob | null
-  fullFile: Blob | null
+  thumbFile?: Blob | null
+  fullFile?: Blob | null
   height: number
   width: number
 }

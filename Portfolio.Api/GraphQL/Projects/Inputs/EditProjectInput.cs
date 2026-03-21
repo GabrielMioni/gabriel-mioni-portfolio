dd7dea3025f06d0177e84b0d807cdda5be69c60c
Gabriel Mioni <gabriel.mioni@gmail.com>
2026-03-21 15:23:03 -0700

    Added EditProjectInput.Images field

--- Portfolio.Api/GraphQL/Projects/Inputs/EditProjectInput.cs
+++ Portfolio.Api/GraphQL/Projects/Inputs/EditProjectInput.cs
@@ -2,10 +2,16 @@
 
 namespace Portfolio.Api.GraphQL.Projects.Inputs;
 
+public record EditProjectImageInput(
+  Guid ProjectImageId,
+  string AltText
+);
+
 public sealed record EditProjectInput(
     Guid Id,
     string? Title,
     string? Summary,
     string? Body,
-    ProjectStatus? Status
+    ProjectStatus? Status,
+    IReadOnlyList<EditProjectImageInput>? Images
 );

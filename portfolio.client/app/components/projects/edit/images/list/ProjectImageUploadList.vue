255623c95f31001e368dda9d0c2b53bee211ed23
Gabriel Mioni <gabriel.mioni@gmail.com>
2026-03-22 11:10:23 -0700

    ProjectImageUploadList uses draggable from vuedraggable, updates item.sort on drag end

--- portfolio.client/app/components/projects/edit/images/list/ProjectImageUploadList.vue
+++ portfolio.client/app/components/projects/edit/images/list/ProjectImageUploadList.vue
@@ -1,35 +1,44 @@
 <script setup lang="ts">
+import draggable from 'vuedraggable'
 import type { ImageEditorItem } from '~/types/images/ImageEditorItem'
 import ProjectImageUploadListItem from '~/components/projects/edit/images/list/ProjectImageUploadListItem.vue'
 
-const items = defineModel<ImageEditorItem[]>('items', { required: true })
-
-defineEmits<{
-  (e: 'remove', clientId: string): void
-}>()
+const items = defineModel<ImageEditorItem[]>({ required: true })
 
 const removeItem = (clientId: string) => {
   const index = items.value.findIndex(item => item.clientId === clientId)
   if (index !== -1) {
     items.value.splice(index, 1)
+    syncSortOrder()
   }
+}
+
+const syncSortOrder = () => {
+  items.value = items.value.map((item, index) => ({
+    ...item,
+    sort: index
+  }))
 }
 
 </script>
 
 <template>
-  <div class="project-image-upload-list">
-    <template
-      v-for="(item, index) in items"
-      :key="item.clientId">
-      <ProjectImageUploadListItem
-        :item="item"
-        @remove="removeItem"/>
-      <v-divider
-        v-if="index !== items.length - 1"
-        class="my-3"/>
+  <draggable
+    v-model="items"
+    item-key="clientId"
+    handle=".drag-handle"
+    @end="syncSortOrder">
+    <template #item="{ element, index }">
+      <div>
+        <ProjectImageUploadListItem
+          :item="element"
+          @remove="removeItem" />
+        <v-divider
+          v-if="index !== items.length - 1"
+          class="my-3" />
+      </div>
     </template>
-  </div>
+  </draggable>
 </template>
 
 <style scoped>

0b9f41abf884ffff286def94846cd87688834c23
Gabriel Mioni <gabriel.mioni@gmail.com>
2026-03-26 19:10:05 -0700

    RemoveImagesDialog uses ProjectImageUploadListItem

--- portfolio.client/app/components/projects/edit/images/RemovedImagesDialog.vue
+++ portfolio.client/app/components/projects/edit/images/RemovedImagesDialog.vue
@@ -3,17 +3,53 @@ import type { ImageEditorItem } from '~/types/images/ImageEditorItem'
 
 const dialog = defineModel<boolean>()
 
-defineProps<{
+const props = defineProps<{
   removedImageItems: ImageEditorItem[]
 }>()
+
+defineEmits<{
+  (e: 'add', clientId: string): void
+}>()
+
+const removedImagesLength = computed(() => props.removedImageItems.length)
+
+watch(
+  removedImagesLength,
+  (val) => {
+    if (val <= 0) {
+      dialog.value = false
+    }
+  }
+)
 
 </script>
 
 <template>
   <BaseDialog
     v-model="dialog"
-    title="Removed Images">
-    Blah
+    hide-toolbar>
+    <template #card-title>
+      <div class="d-flex align-center">
+        <span class="fs-14 font-weight-bold">
+          Removed Images
+        </span>
+        <v-spacer />
+        <v-btn
+          icon="mdi-close"
+          flat
+          small
+          density="compact"
+          @click="dialog = false" />
+      </div>
+    </template>
+    <div>
+      <ProjectImageUploadListItem
+        v-for="item in removedImageItems"
+        :key="item.clientId"
+        :item="item"
+        :is-removing="false"
+        @update="$emit('add', $event)"/>
+    </div>
     <template #actions>
       <v-spacer />
       <v-btn

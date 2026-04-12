9868fb822fc0ca930c4db552cc5147426996d8bc
Gabriel Mioni <gabriel.mioni@gmail.com>
2026-04-12 10:39:07 -0700

    Add LinkTypeIcon component

---
+++ portfolio.client/app/components/projects/edit/links/LinkTypeIcon.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { ProjectLinkType } from '~/generated/graphql'
+
+const props = withDefaults(
+  defineProps<{
+      linkType: ProjectLinkType,
+      color?: string,
+      size?: 'x-small' | 'small' | 'default' | 'large' | 'x-large'
+    }>(),
+  {
+    color: 'primary',
+    size: 'default'
+  }
+)
+
+const icon = computed(() => {
+  switch (props.linkType) {
+  case ProjectLinkType.Demo:
+    return 'mdi-monitor-dashboard'
+  case ProjectLinkType.Repository:
+    return 'mdi-source-repository'
+  default:
+    return 'mdi-link-variant'
+  }
+})
+
+</script>
+
+<template>
+  <v-icon
+    :color="color"
+    :size="size">
+    {{ icon }}
+  </v-icon>
+</template>
+
+<style scoped>
+
+</style>

e06f793ef09d66df0cd0ed4af68a1a4f0ef86ef8
Gabriel Mioni <gabriel.mioni@gmail.com>
2026-04-12 10:05:50 -0700

    Add ProjectLinkSelect component

---
+++ portfolio.client/app/components/projects/edit/links/ProjectLinkSelect.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { ProjectLinkType } from '~/generated/graphql'
+
+const linkType = defineModel<ProjectLinkType>({ required: true })
+
+function formatTitle (linkType: ProjectLinkType): string {
+  switch (linkType) {
+  case ProjectLinkType.Demo:
+    return 'Demo'
+  case ProjectLinkType.Repository:
+    return 'Repository'
+  default:
+    return 'External'
+  }
+}
+
+const selectItems = computed(() => {
+  return Object.values(ProjectLinkType).map((itemType) => ({
+    value: itemType,
+    title: formatTitle(itemType)
+  }))
+})
+</script>
+
+<template>
+  <v-select
+    v-model="linkType"
+    :items="selectItems"
+    variant="filled"
+    hide-details
+    density="compact" />
+</template>
+
+<style scoped>
+
+</style>

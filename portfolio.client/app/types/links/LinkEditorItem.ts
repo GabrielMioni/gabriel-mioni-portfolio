10473d43c092a860ba1c53af6f95cdb5e5cf62e2
Gabriel Mioni <gabriel.mioni@gmail.com>
2026-04-26 12:00:15 -0700

    Add createdAt field on linkEditorItems.ts

--- portfolio.client/app/types/links/LinkEditorItem.ts
+++ portfolio.client/app/types/links/LinkEditorItem.ts
@@ -3,6 +3,7 @@ import type { ProjectLinkType } from '~/generated/graphql'
 export type LinkEditorItem = {
   id?: string | null
   clientId: string
+  createdAt?: string | null
   url: string
   text: string
   type: ProjectLinkType

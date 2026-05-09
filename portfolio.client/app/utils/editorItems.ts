e7a79f32fd52cd24aeb1559b325132e350de0fb4
Gabriel Mioni <gabriel.mioni@gmail.com>
2026-05-09 12:33:42 -0700

    In editorItems.ts setEditorItemRemovedState immediately filters out items that don't have an id

--- portfolio.client/app/utils/editorItems.ts
+++ portfolio.client/app/utils/editorItems.ts
@@ -1,8 +1,7 @@
 import type {
-  HasClientId,
   HasId,
-  HasIsRemoved,
-  HasSort
+  HasSort,
+  BaseEditorItem
 } from '~/types/editor-items'
 
 export const checkIfEditorItemsUpdated = <
@@ -52,28 +51,39 @@ export const normalizeEditorItemsSortOrder = <T extends HasSort>(
   }))
 }
 
-const setEditorItemRemovedState = <T extends HasSort & HasClientId & HasIsRemoved> (
+const setEditorItemRemovedState = <T extends BaseEditorItem>(
   clientId: string,
   items: T[],
   isRemoved: boolean
 ): T[] => {
-  const nextItems = items.map(item =>
-    item.clientId === clientId
-      ? { ...item, isRemoved }
-      : item
-  )
+  const index = items.findIndex(item => item.clientId === clientId)
+
+  if (index < 0) {
+    throw new Error(`Item with clientId ${clientId} not found`)
+  }
+
+  const item = items[index]!
+
+  // If the item doesn't have an id, it means it's not saved yet. Just remove it from the list
+  const nextItems = item.id
+    ? items.map(currentItem =>
+      currentItem.clientId === clientId
+        ? { ...currentItem, isRemoved }
+        : currentItem
+    )
+    : items.filter(currentItem => currentItem.clientId !== clientId)
 
   return normalizeEditorItemsSortOrder(nextItems)
 }
 
-export const removeEditorItem = <T extends HasSort & HasClientId & HasIsRemoved> (
+export const removeEditorItem = <T extends BaseEditorItem> (
   clientId: string,
   items: T[]
 ): T[] => {
   return setEditorItemRemovedState(clientId, items, true)
 }
 
-export const restoreEditorItem = <T extends HasSort & HasClientId & HasIsRemoved>
+export const restoreEditorItem = <T extends BaseEditorItem>
   (
     clientId: string,
     items: T[]

eb5378377302fe664b66bbe00e6fb2a7947e7274
Gabriel Mioni <gabriel.mioni@gmail.com>
2026-05-10 10:21:02 -0700

    pages/projects/create.vue uses ProjectEditor

--- portfolio.client/app/pages/projects/create.vue
+++ portfolio.client/app/pages/projects/create.vue
@@ -1,21 +1,10 @@
 <script setup lang="ts">
-import { ProjectStatus } from '~/generated/graphql'
 
-const form = reactive({
-  title: '',
-  summary: '',
-  body: '',
-  status: ProjectStatus.Draft
-})
-const isValid = ref(false)
 </script>
 
 <template>
   <v-container>
-    <h3>Create Page</h3>
-    <ProjectForm
-      v-model:form="form"
-      v-model:is-valid="isValid" />
+    <ProjectEditor />
   </v-container>
 </template>
 

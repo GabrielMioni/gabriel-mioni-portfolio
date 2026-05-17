a6093a848b5b33d1cf9bbb2272d53ceada010f12
Gabriel Mioni <gabriel.mioni@gmail.com>
2026-05-17 10:58:43 -0700

    In useSnackbarStore rename show method to showSnackbar

--- portfolio.client/app/stores/useSnackbarStore.ts
+++ portfolio.client/app/stores/useSnackbarStore.ts
@@ -3,12 +3,12 @@ import type { SnackbarMessage, SnackbarColor } from '~/types/ui/SnackbarMessage'
 export const useSnackbarStore = defineStore('snackbar', () => {
   const messages = ref<SnackbarMessage[]>([])
 
-  const show = (text: string, color: SnackbarColor = 'info') => {
+  const showSnackbar = (text: string, color: SnackbarColor = 'info') => {
     messages.value.push({ text, color })
   }
 
   return {
     messages,
-    show
+    showSnackbar
   }
 })

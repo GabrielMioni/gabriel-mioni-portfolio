5a226ab338799c7b52adc5a3472a47dc9f2879a7
Gabriel Mioni <gabriel.mioni@gmail.com>
2026-05-16 17:41:38 -0700

    Add GlobalSnackbar.vue

---
+++ portfolio.client/app/components/base-components/GlobalSnackbar.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { storeToRefs } from 'pinia'
+
+const snackbarStore = useSnackbarStore()
+const { messages } = storeToRefs(snackbarStore)
+</script>
+
+<template>
+  <v-snackbar-queue
+    v-model="messages"
+    timeout="3000"
+    closable />
+</template>
+
+<style scoped>
+
+</style>

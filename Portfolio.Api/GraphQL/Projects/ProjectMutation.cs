e9d1df8ffceef8c5418e0625b46efd108c414630
Gabriel Mioni <gabriel.mioni@gmail.com>
2026-05-23 10:20:40 -0700

    In ProjectMutations move method arguments to separate lines for readability

--- Portfolio.Api/GraphQL/Projects/ProjectMutation.cs
+++ Portfolio.Api/GraphQL/Projects/ProjectMutation.cs
@@ -1,15 +1,16 @@
 ﻿using Portfolio.Api.Domain.Projects;
 using Portfolio.Api.GraphQL.Projects.Inputs;
 using Portfolio.Api.GraphQL.Projects.Payloads;
-using Portfolio.Api.GraphQL.Projects.Types;
 using Portfolio.Api.Services;
-using Portfolio.Api.Services.Storage;
 
 namespace Portfolio.Api.GraphQL.Projects
 {
     public class ProjectMutation
     {
-        public Task<Project> CreateProject(CreateProjectInput input, [Service] ProjectService projects, CancellationToken ct = default)
+        public Task<Project> CreateProject(
+            CreateProjectInput input,
+            [Service] ProjectService projects,
+            CancellationToken ct = default)
         {
             return projects.CreateAsync(input, ct);
         }
@@ -23,17 +24,26 @@ namespace Portfolio.Api.GraphQL.Projects
 
             return new DeleteProjectPayload(projectId);
         }
-        public Task<Project?> EditProject(EditProjectInput input, [Service] ProjectService projects, CancellationToken ct = default)
+        public Task<Project?> EditProject(
+            EditProjectInput input,
+            [Service] ProjectService projects,
+            CancellationToken ct = default)
         {
             return projects.EditProjectAsync(input, ct);
         }
 
-        public Task<Project?> PublishProject(Guid id, [Service] ProjectService projects, CancellationToken ct = default)
+        public Task<Project?> PublishProject(
+            Guid id,
+            [Service] ProjectService projects,
+            CancellationToken ct = default)
         {
             return projects.PublishAsync(id, ct);
         }
 
-        public Task<Project?> ArchiveProject(Guid id, [Service] ProjectService projects, CancellationToken ct = default)
+        public Task<Project?> ArchiveProject(
+            Guid id,
+            [Service] ProjectService projects,
+            CancellationToken ct = default)
         {
             return projects.ArchiveAsync(id, ct);
         }       

ff4957d9d483401625bd7ff376d4f676286beb70
Gabriel Mioni <gabriel.mioni@gmail.com>
2026-05-31 15:36:29 -0700

    codegen.ts retrieves schema from graphql/admin

--- portfolio.client/codegen.ts
+++ portfolio.client/codegen.ts
@@ -1,7 +1,7 @@
 import type { CodegenConfig } from '@graphql-codegen/cli'
 
 const config: CodegenConfig = {
-  schema: 'http://localhost:5217/graphql',
+  schema: 'http://localhost:5217/graphql/admin',
   documents: ['app/graphql/**/*.gql'],
   generates: {
     './app/generated/': {

bfa7c6707ebff4d797350ee63115ce4744100e20
Gabriel Mioni <gabriel.mioni@gmail.com>
2026-06-06 11:04:51 -0700

    Setup /graphql proxy in nuxt.config.ts

--- Portfolio.Public/nuxt.config.ts
+++ Portfolio.Public/nuxt.config.ts
@@ -4,23 +4,29 @@ export default defineNuxtConfig({
     '@nuxt/eslint',
     '@nuxt/ui'
   ],
-
   devtools: {
     enabled: true
   },
-
-  devServer: {
-    port: 3001
-  },
-
   css: ['~/assets/css/main.css'],
-
+  runtimeConfig: {
+    public: {
+      graphQlBase: process.env.NUXT_PUBLIC_GRAPHQL_BASE || '/graphql'
+    }
+  },
   routeRules: {
     '/': { prerender: true }
   },
-
+  devServer: {
+    port: 3001
+  },
   compatibilityDate: '2025-01-15',
-
+  nitro: {
+    routeRules: {
+      '/graphql': {
+        proxy: 'http://localhost:5217/graphql'
+      }
+    }
+  },
   eslint: {
     config: {
       stylistic: {

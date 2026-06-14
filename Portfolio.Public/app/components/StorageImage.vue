a6f660fee8e8ae4a18e037e54108d8b93f407392
Gabriel Mioni <gabriel.mioni@gmail.com>
2026-06-14 10:32:44 -0700

    Add StorageImage.vue

---
+++ Portfolio.Public/app/components/StorageImage.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const config = useRuntimeConfig()
+
+const props = defineProps<{
+  alt?: string | null
+  storageKey: string
+}>()
+
+const src = computed(() => `${config.public.storageBase}/${props.storageKey}`)
+</script>
+
+<template>
+  <img
+    :src="src"
+    :alt="alt ?? ''"
+    loading="lazy"
+    class="block mx-auto object-center"
+    v-bind="$attrs">
+</template>

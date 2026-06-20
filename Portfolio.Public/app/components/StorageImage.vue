3b4778739c70d45f0cfbc0fde3c49680d785b8fa
Gabriel Mioni <gabriel.mioni@gmail.com>
2026-06-20 10:52:58 -0700

    In StorageImage add height/width props with default values

--- Portfolio.Public/app/components/StorageImage.vue
+++ Portfolio.Public/app/components/StorageImage.vue
@@ -1,19 +1,30 @@
 <script setup lang="ts">
 const config = useRuntimeConfig()
 
-const props = defineProps<{
-  alt?: string | null
-  storageKey: string
-}>()
+const props = withDefaults(
+  defineProps<{
+    alt?: string
+    storageKey: string
+    height?: string | number
+    width?: string | number
+  }>(),
+  {
+    alt: 'project image',
+    height: 'auto',
+    width: 'auto'
+  }
+)
 
 const src = computed(() => `${config.public.storageBase}/${props.storageKey}`)
 </script>
 
 <template>
   <img
+    v-bind="$attrs"
+    :alt="alt"
+    :height="height"
+    :width="width"
     :src="src"
-    :alt="alt ?? ''"
     loading="lazy"
-    class="block mx-auto object-center"
-    v-bind="$attrs">
+    class="block mx-auto object-center">
 </template>

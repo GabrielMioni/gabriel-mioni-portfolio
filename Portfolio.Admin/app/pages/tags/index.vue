a1de9d412bb77e2bf6e6cdefe78abfda840aa12e
Gabriel Mioni <gabriel.mioni@gmail.com>
2026-07-04 11:31:45 -0700

    Add BaseTable for tags

--- Portfolio.Admin/app/pages/tags/index.vue
+++ Portfolio.Admin/app/pages/tags/index.vue
@@ -1,5 +1,40 @@
+<script setup lang="ts">
+import type { ProjectTagFragment } from '~/generated/graphql'
+import type { Header, TableOptions } from '~/types/ui/datatable'
+
+type TagKey = keyof ProjectTagFragment
+type TagHeader = Header<TagKey>
+
+const { allTags, fetchingTags } = useProjectTagQueries()
+
+const options = ref<TableOptions>({
+  page: 1,
+  itemsPerPage: -1,
+  sortBy: [{ key: 'name', order: 'asc' }],
+  groupBy: []
+})
+
+const headers: TagHeader[] = [
+  { title: 'Name', key: 'name', sortable: true, align: 'start' },
+  { title: 'Value', key: 'value', sortable: true, align: 'start' }
+]
+</script>
+
 <template>
   <v-container>
-    <h1>Tags</h1>
+    <BaseTable
+      :options="options"
+      :headers="headers"
+      :items="allTags"
+      :items-length="allTags.length"
+      density="comfortable"
+      @update:options="options = $event">
+      <template #item="{ item }">
+        <tr style="cursor: pointer">
+          <td v-text="item.name" />
+          <td v-text="item.value" />
+        </tr>
+      </template>
+    </BaseTable>
   </v-container>
 </template>

d34921bd57f2c2652b297488622dc4ecdf2895c9
Gabriel Mioni <gabriel.mioni@gmail.com>
2026-07-16 19:14:43 -0700

    Add utils/tagIcons.ts

---
+++ Portfolio.Public/app/utils/tagIcons.ts
@@ -0,0 +1,8 @@
+export const tagIconMap: Record<string, string> = {
+  'vue': 'i-simple-icons-vuedotjs',
+  'typescript': 'i-simple-icons-typescript',
+  'nuxt': 'i-simple-icons-nuxtdotjs',
+  '.net': 'i-simple-icons-dotnet',
+  'c#': 'i-simple-icons-csharp',
+  'graphql': 'i-simple-icons-graphql'
+}
